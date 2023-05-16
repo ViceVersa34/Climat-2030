312e30d80d01128cef6bb240d97af23aa464e1a2
Enzobu <enzo.palermo@epsi.fr>
2023-05-16 13:16:00 +0200

    Update 404.php

--- fr/404.php
+++ fr/404.php
@@ -1,1 +1,27 @@
-r
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <?php include('php/head.php'); ?>
+    <link rel="stylesheet" href="/css/404.css">
+    <title>404 - Climat-2030</title>
+</head>
+<body id="body">
+    <header>
+        <?php include('php/header.php'); ?>
+    </header>
+
+    <main>
+
+    <div class="404">Il n'y a rien par ici !</div>
+
+    <div class="scrollUp">
+        <a href="#top"><img src="/img/up-arrow.svg"/></a>
+    </div>
+
+    </main>
+
+    <footer>
+        <?php include('php/footer.php'); ?>
+    </footer>
+</body>
+</html>
