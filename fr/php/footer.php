85a52bab716eaf738caa082098c3ecc2d5a1ddeb
Enzobu <enzo.palermo@epsi.fr>
2023-05-16 10:56:12 +0200

    add fr/en

--- fr/php/footer.php
+++ fr/php/footer.php
@@ -4,17 +4,17 @@
         <div class="we-can-do-it">
             <img src="/img/we_can_do_it" alt="illustration 'we can do it'" class="we-can-do-it-img">
             <div class="we-can-do-it-description">
-                <a href="">En savoir plus sur Climat-2030</a>
+                <a href="/fr/">En savoir plus sur Climat-2030</a>
             </div>
         </div>
-    </div>
+    </div>  
     <div class="footer-bottom">
         <div class="mention-legales">
             Ce site est édité et publier par <br> 
             Estelle Couderc-Doat [Tout droits réservés] <br> 
             © Climat-2030 <br> 
             2023 <br>
-            <a href="/mention-legales.php">Mention légales</a>
+            <a href="/fr/mention-legales.php">Mention légales</a>
         </div>
         <img src="/img/generation_egalite" alt="logo generation égalité" class="gen-egal">
     </div>
