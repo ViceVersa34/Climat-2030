2d39cdb9a9171e8672e2c529c1ef1b60ba8b2fd5
Enzobu <enzo.palermo@epsi.fr>
2023-05-17 14:02:54 +0200

    replace content for page 2

--- fr/academie-du-climat.php
+++ fr/academie-du-climat.php
@@ -2,7 +2,7 @@
 <html lang="fr">
 <head>
     <?php include('php/head.php'); ?>
-    <link rel="stylesheet" href="/css/index.css">
+    <link rel="stylesheet" href="/css/academie-du-climat.css">
     <title>Climat-2030</title>
 </head>
 <body id="body">
@@ -12,63 +12,55 @@
 
     <main>
 
-    <h1 class="top_txt">Métamorphoser l'humanité en virtuose du climat</h1>
-    <a href="" class="en-savoir-plus">En savoir +</a>
+    <h1 class="top_txt">Académie du climat-2030</h1>
+    <a href="" class="en-savoir-plus">S'inscrire</a>
 
     <div class="top-illustration">
-        <img src="/img/earth_day.jpg" alt="Illustration - 'earth day'">
-        <img src="/img/earth_day.jpg" alt="Illustration - 'earth day'">
-        <img src="/img/earth_day.jpg" alt="Illustration - 'earth day'">
+        <img src="/img/academie_du_climat/oiseau.jpg" alt="Illustration - 'oiseau'">
+        <img src="/img/academie_du_climat/oiseau.jpg" alt="Illustration - 'oiseau'">
+        <img src="/img/academie_du_climat/oiseau.jpg" alt="Illustration - 'oiseau'">
+        <img src="/img/academie_du_climat/oiseau.jpg" alt="Illustration - 'oiseau'">
     </div>
 
     <div class="valeurs">
         <div class="valeurs-part1">
             <div class="principe">
                 <p>
-                    <span class="yello-txt size-txt">Pourquoi</span> Si nous arrivons à comprendre le réchauffement climatique, cela changera l'avenir de l'humanité dans le monde.
+                    <span class="yello-txt size-txt">Objectif</span> L'académie s'est fixée comme objectif que chaque personne puisse s'éduquer aux enjeux du climat, quelque soit son niveau de connaissances de départ.
                 </p>
             </div>
             <div class="principe">
                 <p>
-                    <span class="yello-txt size-txt">Comment</span> En mettant l'accent sur <span class="yello-txt">l'émancipation</span> des humains et <span class="yello-txt">des femmes</span> en particulier pour garantir un avenir durable.
-                </p>
-            </div>
-            <div class="principe">
-                <p>
-                    <span class="yello-txt size-txt">Proposition</span> Une éducation aussi puissante qu'originale au climat pour se métamorphoser en virtuose du climat.
+                    <span class="yello-txt size-txt">Modalités</span><br>
+                    Trois niveaux pour monter en compétence : Débutant, Intermédiaire et Confirmé.<br>
+                    L'académie du climat-2030 favorise l'intelligence collective de groupe.<br>
+                    Les séances mixent le jeu, l'interaction et les données issues du GIEC, de l'ADEME et d'autres organisations scientifiques en lien avec Le Climat.
                 </p>
             </div>
         </div>
-
-        <div class="img-principes"><img src="/img/butterfly.jpg" alt="butterfly"></div>
-        <div class="valeurs-part2">
-            <div class="principe sl">
-                <p>
-                    <span class="yello-txt size-txt">Vision</span> Un monde où chaque personne a <span class="yello-txt">conscience</span> de la nécessité de limiter le réchauffement climatique à 1,50C d'ici 2030 et au delà. <br>
-                    Pour cela, il faut mettre à disposition de chaque cerveau humain la passion pour l'érudition et la capacité d'innovation pour faire de "2 tonnes de carbone" une habitude de pensée quotidienne.
-                </p>
+        <div class="programmes">
+            <div class="desc-programmes">
+                <div class="desc">Programme de formation au climat</div>
+                <a href="#levelA" class="lien-programmes">Débutant</a>
+                <a href="#levelB" class="lien-programmes">Intermédiaire</a>
+                <a href="#levelC" class="lien-programmes">Confirmé</a>
+                <div class="repere" id="levelA"></div>
             </div>
-            <div class="principe sl">
-                <p>
-                    <span class="yello-txt size-txt">Mission</span> Encourager les femmes et les hommes à prendre leur place pour parvenir à contenir l'objectif de réchauffement climatique à 1,50C...en créant 
-                    un programme d'éducation sur le climat pour respecter l'échéance de <span class="yello-txt">l'humanité</span>.
-                </p>
-            </div>
-            <div class="yello-txt size-txt pr valeur">Valeurs ?</div>
-            <div class="penser-climat">
-                <div class="yello-txt size-txt pr penser-le-climat">Penser le climat</div>
-                <div class="pensee"><span class="yello-txt size-txt pr">C</span> ommencer par soi</div>
-                <div class="pensee"><span class="yello-txt size-txt pr">L</span> ibérer (se) des habitudes passées</div>
-                <div class="pensee"><span class="yello-txt size-txt pr">I</span> nvestir du temps pour s'éduquer</div>
-                <div class="pensee"><span class="yello-txt size-txt pr">M</span> aintenant. Plus tard n'est plus une option</div>
-                <div class="pensee"><span class="yello-txt size-txt pr">A</span> dapter son comportement à l'urgence climatique</div>
-                <div class="pensee"><span class="yello-txt size-txt pr">T</span> riompher collectivement !</div>
+            <div class="programme">
+                <div class="programme-img">
+                    <img src="/img/academie_du_climat/levelA" alt="Programme niveau débutant">
+                    <div class="repere" id="levelB"></div>
+                </div>
+                <div class="programme-img">
+                    <img src="/img/academie_du_climat/levelB" alt="Programme niveau intermédiaire">
+                    <div class="repere" id="levelC"></div>
+                </div>
+                <div class="programme-img">
+                    <img src="/img/academie_du_climat/levelC" alt="Programme niveau confirmé">
+                </div>
             </div>
         </div>
     </div>
-
-
-
 
     <div class="scrollUp">
         <a href="#top"><img src="/img/up-arrow.svg"/></a>
