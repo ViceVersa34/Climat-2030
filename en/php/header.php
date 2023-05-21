6167e475ff0f71ce3ba04bab1e08b8e0e293de69
Enzobu <enzo.palermo@epsi.fr>
2023-05-21 14:27:01 +0200

    header traduction

--- en/php/header.php
+++ en/php/header.php
@@ -4,16 +4,16 @@
     <a href="/en/" class="img-logo"><img src="/img/logo.png" alt="logo Climat-2030"></a>
     <ul class="nav-list" id="nav">
         <li class="arrow-menu"><button class="arrow-menu-burger" onclick="changerLeft2('-20rem');"><img src="/img/croix.svg" alt="Menu burger"></button></li>
-        <li class="nav-item"><a href="/en/">Accueil</a></li>
-        <li class="nav-item"><a href="/en/the-climate-2030-academy.php">L'académie du climat</a></li>
-        <li class="nav-item"><a href="/en/the-social-club-network.php">Le Social Club</a></li>
-        <li class="nav-item"><a href="/en/the-exhibition.php">L'exposition</a></li>
-        <li class="nav-item"><a href="/en/the-humour.php">L'humour</a></li>
-        <li class="nav-item"><a href="/en/sign-up.php">S'inscrire</a></li>
+        <li class="nav-item"><a href="/en/">Home</a></li>
+        <li class="nav-item"><a href="/en/the-climate-2030-academy.php">The Climate-2023 Academy</a></li>
+        <li class="nav-item"><a href="/en/the-social-club-network.php">The social Club Network</a></li>
+        <li class="nav-item"><a href="/en/the-exhibition.php">The Exhibition</a></li>
+        <li class="nav-item"><a href="/en/the-humour.php">The Humour</a></li>
+        <li class="nav-item"><a href="/en/sign-up.php">Sign-in</a></li>
         <li class="nav-item">
             <ul class="language">
-                <li class="nav-item-language active"><a href="/fr/">FR</a></li>
-                <li class="nav-item-language"><a href="/en/">EN</a></li>
+                <li class="nav-item-language"><a href="/fr/">FR</a></li>
+                <li class="nav-item-language active"><a href="/en/">EN</a></li>
             </ul>
         </li>
     </ul>
