8a9b7347ab02b00f42e9a1b83319f96ddd2bb68c
Enzobu <enzo.palermo@epsi.fr>
2023-05-21 15:22:05 +0200

    exhibition traduction

--- en/the-exhibition.php
+++ en/the-exhibition.php
@@ -12,8 +12,8 @@
 
     <main>
 
-    <h1 class="top_txt">L'exposition <span class="orange-txt size-txt2 txt-g font-family">"La grande histoire du climat"</span></h1>
-    <a href="/en/sign-up.php#exposition" class="en-savoir-plus">Demande de devis</a>
+    <h1 class="top_txt">The exhibition <span class="orange-txt size-txt2 txt-g font-family">“The great history of climate change”</span></h1>
+    <a href="/en/sign-up.php#exposition" class="en-savoir-plus">Request a quote</a>
 
     <div class="top-illustration">
         <img src="/img/exposition/illustration_exposition.png" alt="Illustration exposition">
@@ -23,13 +23,14 @@
         <div class="valeurs-part1">
             <div class="principe">
                 <p>
-                    <span class="yello-txt size-txt">Objectif</span> Comprendre les origines historiques du réchauffement climatique et ses répercussions actuelles et à venir.
+                    <span class="yello-txt size-txt">Goal</span> Understand the historical origins of global warming and its current and future impacts.
                 </p>
             </div>
             <div class="principe">
                 <p>
-                    <span class="yello-txt size-txt">Comment</span> Exposition de frises chronologiques expliquant les mécanismes historiques, politiques, économiques, scientifiques
-                    et artistiques qui ont conduit à notre situation actuelle où contenir le réchauffement climatique à 1,50C d'ici 2030 et au delà est devenu une nécessité absolue.
+                    <span class="yello-txt size-txt">How</span> Exhibition of chronological friezes explaining the historical, political, economic, scientific 
+                    and artistic mechanisms that led to our current situation where containing global warming to 1.5°C by 2030 and beyond has become an absolute 
+                    necessity.
                 </p>
             </div>
         </div>
