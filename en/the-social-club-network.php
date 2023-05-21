bf911ee5403a131296eec5dc027fe4098de886a5
Enzobu <enzo.palermo@epsi.fr>
2023-05-21 15:31:10 +0200

    social club traduction

--- en/the-social-club-network.php
+++ en/the-social-club-network.php
@@ -12,8 +12,8 @@
 
     <main>
 
-    <h1 class="top_txt">Le réseau <span class="orange-txt size-txt2 txt-g font-family">Social Club</span></h1>
-    <a href="/en/sign-up.php#social-club" class="en-savoir-plus">Pré-inscription</a>
+    <h1 class="top_txt"><span class="orange-txt size-txt2 txt-g font-family">The Social Club </span>network</h1>
+    <a href="/en/sign-up.php#social-club" class="en-savoir-plus">Pre-booking</a>
 
     <div class="top-illustration">
         <img src="/img/social_club/illustration_social_club.png" alt="Illustration social club">
@@ -23,24 +23,24 @@
         <div class="valeurs-part1">
             <div class="principe">
                 <p>
-                    <span class="yello-txt size-txt">Objectif</span> S'informer et agir pour soi ou collectivement pour réussir à transformer notre futur en un avenir viable.
+                    <span class="yello-txt size-txt">Aim</span> Learn and act individually or collectively to succeed in metamorphosing our future into a sustainable one.
                 </p>
             </div>
             <div class="principe">
                 <p>
-                    <span class="yello-txt size-txt">Comment</span> En étant membre du Social Club.
+                    <span class="yello-txt size-txt">How</span> Become a member of the Social Club.
                 </p>
             </div>
             <div class="principe">
                 <p>
-                    <span class="yello-txt size-txt">Pourquoi un réseau social</span> Créer du lien entre virtuoses du climat est ressenti comme nécessaire pour se motiver, 
-                    se soutenir pour contenir le réchauffement climatique à 1,50C à l'horizon 2030 et au delà.
+                    <span class="yello-txt size-txt">Why a social network</span> Create a link between climate virtuosos is felt as a necessity to motivate and support 
+                    each other to contain global warming to 1.5°C by 2030 and beyond.
                 </p>
             </div>
             <div class="principe">
                 <p>
-                    <span class="yello-txt size-txt">Se connecter</span> Pour rester proches à distance. Pour s'inspirer mutuellement ou agir ensemble, réuni.e.s par leur 
-                    talent et leur volonté de contenir le réchauffement climatique à 1,50C à l'horizon 2030 et au delà.
+                    <span class="yello-txt size-txt">Connect</span> Stay close remotely. To inspire each other or act together, through the talents and willingness to 
+                    contain global warming to 1.5°C by 2030 and beyond.
                 </p>
             </div>
         </div>
@@ -51,24 +51,25 @@
         <div class="infos">
             <div class="info">
                 <p>
-                    <span class="orange-txt size-txt font-family">Un fil d'actualité</span> pour s'informer sur les initiatives individuelles et collectives qui font avancer l'éducation de
-                    toutes et tous aux enjeux du climat; les événements importants, les pastilles historiques ou humoristiques autour sur climat.
+                    <span class="orange-txt size-txt font-family">News feed</span> to learn about individual and collective initiatives that make the education of all go 
+                    further about global warming issues; about important events, historical or humorous climate information.
+
                 </p>
             </div>
             <div class="info">
                 <p>
-                    <span class="orange-txt size-txt font-family">Des actions</span> pour agir collectivement avec la possibilité de sfinscrire, seul ou en groupe, au niveau au niveau local, 
-                    national ou international.
+                    <span class="orange-txt size-txt font-family">Actions</span> to act collectively with the possibility to register, alone or as a group, at the local, 
+                    national or international level.
                 </p>
             </div>
             <div class="info">
                 <p>
-                    <span class="orange-txt size-txt font-family">Un agenda</span> pour partager les événements importants et les actions du social club.
+                    <span class="orange-txt size-txt font-family">Agenda</span> to share important events and actions of the Social Club network.
                 </p>
             </div>
             <div class="info">
                 <p>
-                    <span class="orange-txt size-txt font-family">Une connexion</span> aux autre virtuoses en fonction des centres d'intérêts communs.
+                    <span class="orange-txt size-txt font-family">Connection</span> to other virtuosos according to their common interests.
                 </p>
             </div>
         </div>
