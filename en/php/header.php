b014057d298e04f9ba2cac2df884339ef77a153e
Enzobu <enzo.palermo@epsi.fr>
2023-05-21 14:18:55 +0200

    update link for en pages

--- en/php/header.php
+++ en/php/header.php
@@ -1,7 +1,7 @@
 <div class="nav">
     <div class="blur" id="blur" onclick="changerLeft2('-18rem');"></div> <!-- Cache permettant de sortir du menu (en mode responsive) lors d'un click a côté -->
     <button class="menu-burger" onclick="changerLeft('0');"><img src="/img/menu.svg" alt="Menu burger"></button>
-    <a href="/" class="img-logo"><img src="/img/logo.png" alt="logo Climat-2030"></a>
+    <a href="/en/" class="img-logo"><img src="/img/logo.png" alt="logo Climat-2030"></a>
     <ul class="nav-list" id="nav">
         <li class="arrow-menu"><button class="arrow-menu-burger" onclick="changerLeft2('-20rem');"><img src="/img/croix.svg" alt="Menu burger"></button></li>
         <li class="nav-item"><a href="/en/">Accueil</a></li>
