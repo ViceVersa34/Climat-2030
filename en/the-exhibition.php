86c94a0c775911fbea76eeee9b6facdc01eb6c54
Enzobu <enzo.palermo@epsi.fr>
2023-05-21 14:06:53 +0200

    rename file for en

---
+++ en/the-exhibition.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<head>
+    <?php include('php/head.php'); ?>
+    <link rel="stylesheet" href="/css/exposition.css">
+    <title>Climat-2030</title>
+</head>
+<body id="body">
+    <header>
+        <?php include('php/header.php'); ?>
+    </header>
+
+    <main>
+
+    <h1 class="top_txt">L'exposition <span class="orange-txt size-txt2 txt-g font-family">"La grande histoire du climat"</span></h1>
+    <a href="/fr/inscription.php#exposition" class="en-savoir-plus">Demande de devis</a>
+
+    <div class="top-illustration">
+        <img src="/img/exposition/illustration_exposition.png" alt="Illustration exposition">
+    </div>
+
+    <div class="valeurs">
+        <div class="valeurs-part1">
+            <div class="principe">
+                <p>
+                    <span class="yello-txt size-txt">Objectif</span> Comprendre les origines historiques du réchauffement climatique et ses répercussions actuelles et à venir.
+                </p>
+            </div>
+            <div class="principe">
+                <p>
+                    <span class="yello-txt size-txt">Comment</span> Exposition de frises chronologiques expliquant les mécanismes historiques, politiques, économiques, scientifiques
+                    et artistiques qui ont conduit à notre situation actuelle où contenir le réchauffement climatique à 1,50C d'ici 2030 et au delà est devenu une nécessité absolue.
+                </p>
+            </div>
+        </div>
+    </div>
+
+    <div class="scrollUp">
+        <a href="#top"><img src="/img/up-arrow.svg"/></a>
+    </div>
+
+    </main>
+
+    <footer>
+        <?php include('php/footer.php'); ?>
+    </footer>
+</body>
+</html>
