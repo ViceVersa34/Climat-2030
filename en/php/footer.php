398de00be093c77971a4ae61138115bedebdf6f3
Enzobu <enzo.palermo@epsi.fr>
2023-05-22 15:57:35 +0200

    en footer

--- en/php/footer.php
+++ en/php/footer.php
@@ -1,21 +1,28 @@
 <div class="footer">
-    <div class="footer-top">
-        <img src="/img/contact.png" alt="contact banner" class="bandeau-contact">
-        <div class="we-can-do-it">
-            <img src="/img/we_can_do_it" alt="illustration 'we can do it'" class="we-can-do-it-img">
-            <div class="we-can-do-it-description">
-                <a href="/en/more-about-climat-2030.php">Learn more about Climat-2030</a>
+    <div class="top">
+        <div class="left">
+            <img src="/img/we_can_do_it.png" alt="illustration 'we can do it'">
+        </div>
+        <div class="center">
+            <div class="lien-utile">
+                <div class="lien-utile-item">Climat-2030</div>
+                <div class="lien-utile-item"><a href="/fr/en-savoir-plus.php">Learn more about Climat-2030</a></div>
+                <div class="lien-utile-item"><a href="https://www.instagram.com/climat.2030/" target="blank">Instagram</a></div>
+                <div class="lien-utile-item"><a href="https://www.linkedin.com/" target="blank">Linkedin</a></div>
+                <div class="lien-utile-item"><a href="/fr/contact">Contact</a></div>
             </div>
         </div>
-    </div>  
-    <div class="footer-bottom">
-        <div class="mention-legales">
-            This website is edited and published by <br> 
-            Estelle Couderc-Doat [All rights reserved] <br> 
-            © Climat-2030 <br> 
-            2023 <br>
-            <a href="/en/egal notices.php">Legal notices</a>
+        <div class="right">
+            <div class="label">
+                <div class="txt">Label</div>
+                <img src="/img/generation_egalite.svg" alt="logo génération égalité">
+            </div>
         </div>
-        <img src="/img/generation_egalite" alt="logo generation égalité" class="gen-egal">
+    </div>
+    <div class="bottom">
+        <div class="line"></div>
+        <a class="legal" href="">
+            © Climat-2030 - 2023 | Estelle Couderc-Doat
+        </a>
     </div>
 </div>
