ff25216f03bf8139a86653f3a6d0f97e2664feb2
Enzobu <enzo.palermo@epsi.fr>
2023-05-22 15:56:07 +0200

    update footer

--- fr/php/footer.php
+++ fr/php/footer.php
@@ -1,20 +1,28 @@
 <div class="footer">
-    <div class="left">
-        <img src="/img/we_can_do_it.png" alt="illustration 'we can do it'">
-    </div>
-    <div class="center">
-        <div class="lien-utile">
-            <div class="lien-utile-item">Climat-2030</div>
-            <div class="lien-utile-item"><a href="/fr/en-savoir-plus.php">En savoir plus sur Climat-2030</a></div>
-            <div class="lien-utile-item"><a href="https://www.instagram.com/climat.2030/" target="blank">Instagram</a></div>
-            <div class="lien-utile-item"><a href="https://www.linkedin.com/" target="blank">Linkedin</a></div>
-            <div class="lien-utile-item"><a href="/fr/contact">Contact</a></div>
+    <div class="top">
+        <div class="left">
+            <img src="/img/we_can_do_it.png" alt="illustration 'we can do it'">
+        </div>
+        <div class="center">
+            <div class="lien-utile">
+                <div class="lien-utile-item">Climat-2030</div>
+                <div class="lien-utile-item"><a href="/fr/en-savoir-plus.php">En savoir plus sur Climat-2030</a></div>
+                <div class="lien-utile-item"><a href="https://www.instagram.com/climat.2030/" target="blank">Instagram</a></div>
+                <div class="lien-utile-item"><a href="https://www.linkedin.com/" target="blank">Linkedin</a></div>
+                <div class="lien-utile-item"><a href="/fr/contact">Contact</a></div>
+            </div>
+        </div>
+        <div class="right">
+            <div class="label">
+                <div class="txt">Label</div>
+                <img src="/img/generation_egalite.svg" alt="logo génération égalité">
+            </div>
         </div>
     </div>
-    <div class="right">
-        <div class="label">
-            <div class="txt">Label</div>
-            <img src="/img/generation_egalite.svg" alt="logo génération égalité">
-        </div>
+    <div class="bottom">
+        <div class="line"></div>
+        <a class="legal" href="">
+            © Climat-2030 - 2023 | Estelle Couderc-Doat
+        </a>
     </div>
 </div>
