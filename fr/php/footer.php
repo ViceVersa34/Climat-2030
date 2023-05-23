85a9f43ecb939de509e2b55c0956062c7fddb870
Enzobu <enzo.palermo@epsi.fr>
2023-05-23 14:05:41 +0200

    CGV link

--- fr/php/footer.php
+++ fr/php/footer.php
@@ -21,7 +21,7 @@
     </div>
     <div class="bottom">
         <div class="line"></div>
-        <a class="legal" href="">
+        <a class="legal" href="/fr/CGU.php">
             © Climat-2030 - 2023 | Estelle Couderc-Doat
         </a>
     </div>
