7309d8762f265b1517d3e16c237681588d969e39
Enzobu <enzo.palermo@epsi.fr>
2023-05-24 11:59:32 +0200

    delete img + create programme fro scratch

--- en/the-climate-2030-academy.php
+++ en/the-climate-2030-academy.php
@@ -38,27 +38,148 @@
                 </p>
             </div>
         </div>
-        <div class="programmes">
-            <div class="desc-programmes">
-                <div class="desc">Climate Training Program</div>
-                <div class="lien-programmes">
-                    <a href="#levelA" class="lien-programme">Beginner</a>
-                    <a href="#levelB" class="lien-programme">Intermediate</a>
-                    <a href="#levelC" class="lien-programme">Confirmed</a>
-                </div>
-                <div class="repere" id="levelA"></div>
+    </div>
+    <div class="programmes">
+        <div class="desc-programmes">
+            <div class="desc">Programme de formation au climat</div>
+            <div class="lien-programmes">
+                <a href="#levelA" class="lien-programme">Débutant</a>
+                <a href="#levelB" class="lien-programme">Intermédiaire</a>
+                <a href="#levelC" class="lien-programme">Confirmé</a>
             </div>
-            <div class="programme">
-                <div class="programme-img">
-                    <img src="/img/academie_du_climat/levelA_en.png" alt="Programme niveau débutant">
-                    <div class="repere" id="levelB"></div>
+            <div class="repere" id="levelA"></div>
+        </div>
+        <div class="programme">
+            <div class="programme-img">
+                <div class="level levelA">
+                    <div class="txt">
+                        <div class="level-content titre">
+                            <p>
+                                A - Beginner : Serious Game
+                            </p>
+                        </div>
+                        <div class="level-content objectif">
+                            <p>
+                                <span>Goal :</span> Understand climate and the need to contain warming to 1.5°C.
+                            </p>
+                        </div>
+                        <div class="level-content competences">
+                            <p>
+                                <span>Acquired skills :</span> <br>
+                                At the end of this level, I will be able to understand and explain to my
+                                network the climate crisis and the measures I can take to have a
+                                positive impact.
+                            </p>
+                        </div>
+                        <div class="level-content duree">
+                            <p>
+                                <span>Duration :</span> weeks of training
+                            </p>
+                        </div>
+                        <div class="level-content A">
+                            <p>
+                                <span>A-1 :</span> Introduction to Global Warming - <br>
+                                1 week in person
+                            </p>
+                        </div>
+                        <div class="level-content A">
+                            <p>
+                                <span>A-2 :</span> Introduction to climate transition - <br>
+                                3 weeks remote
+                            </p>
+                        </div>
+                    </div>
+                    <div class="img">
+                        <img src="/img/illustration_level.png" alt="Illustration programmes">
+                    </div>
                 </div>
-                <div class="programme-img">
-                    <img src="/img/academie_du_climat/levelB_en.png" alt="Programme niveau intermédiaire">
-                    <div class="repere" id="levelC"></div>
+                <div class="repere" id="level levelB"></div>
+            </div>
+            <div class="programme-img">
+                <div class="level levelB">
+                    <div class="txt">
+                        <div class="level-content titre">
+                            <p>
+                                B- Intermediate : Interactive learning
+                            </p>
+                        </div>
+                        <div class="level-content objectif">
+                            <p>
+                                <span>Goal :</span> Learning the basics to adapt our behaviour.
+                            </p>
+                        </div>
+                        <div class="level-content competences">
+                            <p>
+                                <span>Acquired skills :</span> <br>
+                                At the end of this level, I will be able to follow my impact in real time
+                                and demystify the climate with my entourage.
+                            </p>
+                        </div>
+                        <div class="level-content duree">
+                            <p>
+                                <span>Duration :</span> 8 weeks of training
+                            </p>
+                        </div>
+                        <div class="level-content B">
+                            <p>
+                                <span>B-1 :</span> Biodiversity and Living - <br>
+                                4 weeks remote
+                        </div>
+                        <div class="level-content B">
+                            <p>
+                                <span>B-2 :</span> Consumering and Energy - <br>
+                                4 weeks remote
+                            </p>
+                        </div>
+                    </div>
+                    <div class="img">
+                        <img src="/img/illustration_level.png" alt="Illustration programmes">
+                    </div>
                 </div>
-                <div class="programme-img">
-                    <img src="/img/academie_du_climat/levelC_en.png" alt="Programme niveau confirmé">
+                <div class="repere" id="level levelC"></div>
+            </div>
+            <div class="programme-img">
+                <div class="level levelC">
+                    <div class="txt">
+                        <div class="level-content titre">
+                            <p>
+                                C- Confirmed : Interactive Learning
+                            </p>
+                        </div>
+                        <div class="level-content objectif">
+                            <p>
+                                <span>Purpose:</span> Metamorphosis into climate virtuosos.
+                            </p>
+                        </div>
+                        <div class="level-content competences">
+                            <p>
+                                <span>Acquired Skills :</span> <br>
+                                I Will be able to be an educator and influencer and
+                                demonstrate that I am a virtuoso by the way I live and communicate.
+                            </p>
+                        </div>
+                        <div class="level-content duree">
+                            <p>
+                                <span>Duration :</span> 8 weeks of training
+                            </p>
+                        </div>
+                        <div class="level-content C">
+                            <p>
+                                <span>C-1 :</span> Emancipate ourselves to metamorphosis into virtuoso climate - <br>
+                                4 weeks remote
+                            </p>
+                        </div>
+                        <div class="level-content C">
+                            <p>
+                                <span>C-2 :</span> Acting in its own way to succeed in transforming our future in 2030 - <br>
+                                4 weeks remote
+                            </p>
+                        </div>
+                    </div>
+                    <div class="img">
+                        <img src="/img/illustration_level.png" alt="Illustration programmes">
+                    </div>
+
                 </div>
             </div>
         </div>
