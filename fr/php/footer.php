80a6968d23afa24da1ce7ac5a695e77febad4dd3
Enzobu <enzo.palermo@epsi.fr>
2023-05-26 11:34:14 +0200

    Compteur + style

--- fr/php/footer.php
+++ fr/php/footer.php
@@ -2,30 +2,51 @@
     <div class="top">
         <div class="left">
             <img src="/img/we_can_do_it.png" alt="illustration 'we can do it'">
-            <div id="countdown">
-                <div class="countdown-item">
-                    <span id="years">00</span>
-                    <span>Years</span>
+            <div class="global-countdown">
+                <div class="countdown-text">
+                    Pour parvenir a nos objectifs, il reste :
                 </div>
-                <div class="countdown-item">
-                    <span id="months">00</span>
-                    <span>Months</span>
+                <div id="countdown" class="countdown">
+                    <div class="countdown-item">
+                        <div id="years" class="time font-family font-weight orange-txt">00</div>
+                        <div class="unit">Ans</div>
+                    </div>
+                    <div class="separateur font-family font-weight orange-txt">:</div>
+                    <div class="countdown-item">
+                        <div id="months" class="time font-family font-weight orange-txt">00</div>
+                        <div class="unit">Mois</div>
+                    </div>  
+                    <div class="separateur font-family font-weight orange-txt">:</div>
+                    <div class="countdown-item">
+                        <div id="days" class="time font-family font-weight orange-txt">00</div>
+                        <div class="unit">Jours</div>
+                    </div>
+                    <div class="separateur font-family font-weight orange-txt">:</div>
+                    <div class="countdown-item">
+                        <div id="hours" class="time font-family font-weight orange-txt">00</div>
+                        <div class="unit">Heures</div>
+                    </div>
+                    <div class="separateur font-family font-weight orange-txt">:</div>
+                    <div class="countdown-item">
+                        <div id="minutes" class="time font-family font-weight orange-txt">00</div>
+                        <div class="unit">Minutes</div>
+                    </div>
+                    <div class="separateur font-family font-weight orange-txt">:</div>
+                    <div class="countdown-item">
+                        <div id="seconds" class="time font-family font-weight orange-txt">00</div>
+                        <div class="unit">Secondes</div>
+                    </div>
                 </div>
-                <div class="countdown-item">
-                    <span id="days">00</span>
-                    <span>Days</span>
+                <div class="fin" style="margin-top: .75rem">
+                    Après c'est la fin du monde
                 </div>
-                <div class="countdown-item">
-                    <span id="hours">00</span>
-                    <span>Hours</span>
+            </div>
+            <div class="global-counter">
+                <div class="counter font-family font-weight orange-txt">
+                    12849
                 </div>
-                <div class="countdown-item">
-                    <span id="minutes">00</span>
-                    <span>Minutes</span>
-                </div>
-                <div class="countdown-item">
-                    <span id="seconds">00</span>
-                    <span>Seconds</span>
+                <div class="counter-text">
+                    personnes ont étés sensibilisés
                 </div>
             </div>
             <script src="/js/countdown.js"></script>
