1abe7c73ec5d9ac6a345003dacef71e393ae308a
Enzobu <enzo.palermo@epsi.fr>
2023-05-30 12:15:37 +0200

    Move countdown en page

--- en/index.php
+++ en/index.php
@@ -19,9 +19,7 @@
     </div>
 
     <div class="top-illustration">
-        <img src="/img/index/earth_day.jpg" alt="Illustration - 'earth day'">
-        <img src="/img/index/earth_day.jpg" alt="Illustration - 'earth day'">
-        <img src="/img/index/earth_day.jpg" alt="Illustration - 'earth day'">
+        <img src="/img/index/butterfly.jpg" alt="Illustration - 'earth day'">
     </div>
 
     <div class="valeurs">
@@ -44,7 +42,54 @@
             </div>
         </div>
 
-        <div class="img-principes"><img src="/img/index/butterfly.jpg" alt="butterfly"></div>
+        <div class="global-countdown">
+            <div class="global-counter">
+                <p>
+                    To date, Climate-2030 has raised awareness among
+                    <span class="counter orange-txt font-weight"> 12 </span>
+                    individuals.
+                </p>
+            </div>
+            <div class="countdown-text">
+                Pour parvenir a nos objectifs, nous avons :
+            </div>
+            <div id="countdown" class="countdown">
+                <div class="countdown-top">
+                    <div class="countdown-item">
+                        <div id="years" class="time size-txt2 font-weight orange-txt">00</div>
+                        <div class="unit">YEARS</div>
+                    </div>
+                    <div class="separateur size-txt2 font-weight orange-txt">:</div>
+                    <div class="countdown-item">
+                        <div id="months" class="time size-txt2 font-weight orange-txt">00</div>
+                        <div class="unit">MONTHS</div>
+                    </div>  
+                    <div class="separateur size-txt2 font-weight orange-txt">:</div>
+                    <div class="countdown-item">
+                        <div id="days" class="time size-txt2 font-weight orange-txt">00</div>
+                        <div class="unit">DAYS</div>
+                    </div>
+                </div>
+                <div class="countdown-bottom">
+                    <div class="countdown-item">
+                        <div id="hours" class="time size-txt2 font-weight orange-txt">00</div>
+                        <div class="unit">HOUR</div>
+                    </div>
+                    <div class="separateur size-txt2 font-weight orange-txt">:</div>
+                    <div class="countdown-item">
+                        <div id="minutes" class="time size-txt2 font-weight orange-txt">00</div>
+                        <div class="unit">MINUTES</div>
+                    </div>
+                    <div class="separateur size-txt2 font-weight orange-txt">:</div>
+                    <div class="countdown-item">
+                        <div id="seconds" class="time size-txt2 font-weight orange-txt">00</div>
+                        <div class="unit">SECONDS</div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <script src="/js/countdown.js"></script>
+
         <div class="valeurs-part2">
             <div class="principe sl">
                 <p>
--- en/php/footer.php
+++ en/php/footer.php
@@ -10,54 +10,7 @@
             </div>
         </div>
         <div class="center">
-        <img src="/img/we_can_do_it.png" alt="illustration 'we can do it'">
-            <div class="global-countdown">
-                <div class="global-counter">
-                    <p>
-                        To date, Climate-2030 has raised awareness among
-                        <span class="counter orange-txt font-weight"> 12 </span>
-                        individuals.
-                    </p>
-                </div>
-                <div class="countdown-text">
-                    Pour parvenir a nos objectifs, nous avons :
-                </div>
-                <div id="countdown" class="countdown">
-                    <div class="countdown-top">
-                        <div class="countdown-item">
-                            <div id="years" class="time size-txt2 font-weight orange-txt">00</div>
-                            <div class="unit">YEARS</div>
-                        </div>
-                        <div class="separateur size-txt2 font-weight orange-txt">:</div>
-                        <div class="countdown-item">
-                            <div id="months" class="time size-txt2 font-weight orange-txt">00</div>
-                            <div class="unit">MONTHS</div>
-                        </div>  
-                        <div class="separateur size-txt2 font-weight orange-txt">:</div>
-                        <div class="countdown-item">
-                            <div id="days" class="time size-txt2 font-weight orange-txt">00</div>
-                            <div class="unit">DAYS</div>
-                        </div>
-                    </div>
-                    <div class="countdown-bottom">
-                        <div class="countdown-item">
-                            <div id="hours" class="time size-txt2 font-weight orange-txt">00</div>
-                            <div class="unit">HOUR</div>
-                        </div>
-                        <div class="separateur size-txt2 font-weight orange-txt">:</div>
-                        <div class="countdown-item">
-                            <div id="minutes" class="time size-txt2 font-weight orange-txt">00</div>
-                            <div class="unit">MINUTES</div>
-                        </div>
-                        <div class="separateur size-txt2 font-weight orange-txt">:</div>
-                        <div class="countdown-item">
-                            <div id="seconds" class="time size-txt2 font-weight orange-txt">00</div>
-                            <div class="unit">SECONDS</div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-            <script src="/js/countdown.js"></script>
+            <img src="/img/we_can_do_it.png" alt="illustration 'we can do it'">
         </div>
         <div class="right">
             <div class="label">
