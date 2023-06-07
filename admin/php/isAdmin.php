10716d85d7c7ecdd3f8ffb06a4cb37b68c0f3ada
Enzobu <enzo.palermo@epsi.fr>
2023-06-07 16:08:50 +0200

    Create isAdmin.php

---
+++ admin/php/isAdmin.php
@@ -0,0 +1,14 @@
+<?php
+
+$isAdmin = 0;
+if(!empty($_SESSION)) {
+    if (array_key_exists('isAdmin', $_SESSION)) {
+        if($_SESSION['isAdmin'] == 1) {
+            $isAdmin = 1;
+        }
+    }
+}
+
+if($isAdmin == 0) {
+    header("Refresh: 0;url=/admin/login.php");
+}
