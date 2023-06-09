5404cf7611134c02823ee8826e678305fddcfee8
Enzobu <enzo.palermo@epsi.fr>
2023-06-09 09:05:47 +0200

    js add line

---
+++ admin/js/add.js
@@ -0,0 +1,5 @@
+function ajouter() {
+    event.preventDefault(); // Annule le comportement par défaut du lien ou du formulaire
+    var divElement = document.getElementById("add");
+    divElement.classList.toggle("invisible");
+  }
