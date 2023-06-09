741ab5a1a5499167454099e929aa4525855d5d7c
Enzobu <enzo.palermo@epsi.fr>
2023-06-09 09:06:29 +0200

    Create header.php

---
+++ admin/php/header.php
@@ -0,0 +1,10 @@
+<div class="nav">
+    <a href="/fr/" class="img-logo"><img src="/img/logoV2.00.png" alt="logo Climat-2030"></a>
+    <ul class="nav-list" id="nav">
+        <li class="nav-item"><a href="/admin/">Accueil</a></li>
+        <li class="nav-item"><a href="/admin/tables/users.php">Users</a></li>
+        <li class="nav-item"><a href="/admin/tables/customers.php">Customers</a></li>
+        <li class="nav-item"><a href="/admin/tables/contents.php">Contents</a></li>
+        <li class="nav-item"><a href="/admin/login.php?destroy="><img src="/admin/img/bouton-marche.svg" alt="Déconnection"></a></li>
+    </ul>
+</div>
