837c42cdb00cf38a3726fb98b5db7527c116cfc9
Enzobu <enzo.palermo@epsi.fr>
2023-06-09 14:04:35 +0200

    add fct for users.php

--- admin/js/delete.js
+++ admin/js/delete.js
@@ -4,3 +4,10 @@ function effacerCustomers(id) {
         window.location.href = '/admin/tables/customers.php?action=delete&id=' + id;
     }
 }
+
+function effacerUsers(id) {
+    event.preventDefault(); // Annule le comportement par défaut du lien ou du formulaire
+    if (window.confirm('Êtes-vous sûr de vouloir supprimer la ligne ' + id + ' ?')) {
+        window.location.href = '/admin/tables/users.php?action=delete&id=' + id;
+    }
+}
