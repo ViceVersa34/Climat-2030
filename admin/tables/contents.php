af2f43d4dd23d23933257c455fdcbc5ddb2d9e52
Enzobu <enzo.palermo@epsi.fr>
2023-06-09 09:06:04 +0200

    Update contents.php

---
+++ admin/tables/contents.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <?php include('../php/head.php'); ?>
+    <title>Document</title>
+</head>
+<body>
+    <header>
+        <?php include('../php/header.php'); ?>
+    </header>
+</body>
+</html>
