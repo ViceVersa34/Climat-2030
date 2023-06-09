b2b232a8b5b54ea64a2cdbcb8f10e8b3ac82db24
Enzobu <enzo.palermo@epsi.fr>
2023-06-09 09:07:17 +0200

    Update index.php

--- admin/index.php
+++ admin/index.php
@@ -1,23 +1,17 @@
 <?php
     session_start();
     include('php/isAdmin.php');
-    print_r($_SESSION);
 ?>
 
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="/admin/css/main.css">
+    <?php include('php/head.php'); ?>
     <title>Admin</title>
 </head>
-<body style="display: flex; flex-direction: column;">
-    <a href="/">accueil</a>
-    <a href="/admin/login.php?destroy=">déconnecter</a>
-    <a href="/admin/tables/users.php">Users</a>
-    <a href="/admin/tables/customers.php">Customers</a>
-    <a href="/admin/tables/contents.php">Contents</a>
+<body>
+    <header>
+        <?php include('php/header.php'); ?>
+    </header>
 </body>
 </html>
