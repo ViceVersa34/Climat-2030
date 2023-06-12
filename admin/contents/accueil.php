c1e2eaaddbc647d488cb4a055b058d169814085f
Enzobu <enzo.palermo@epsi.fr>
2023-06-12 16:24:27 +0200

    add code for contents of home

---
+++ admin/contents/accueil.php
@@ -0,0 +1,258 @@
+<div class="page-selector" id="index"></div>
+<h2 class="title-page">Index</h2>
+<div class="contents">
+    <div class="group-content">
+        <div class="content">
+            <div class="language">
+                <?php 
+                    $tempContenu = $contenu['index']['fr']['titre']['contenu'];
+                    $tempId = $contenu['index']['fr']['titre']['id']; 
+                ?>
+                <?php include('repeat.php'); ?>
+            </div>
+            <div class="language">
+                <?php 
+                    $tempContenu = $contenu['index']['en']['titre']['contenu'];
+                    $tempId = $contenu['index']['en']['titre']['id']; 
+                ?>
+                <?php include('repeat.php'); ?>
+            </div>
+        </div>
+    </div>
+
+
+    <div class="group-content">
+        <div class="content">
+            <div class="language">
+                <?php
+                    $tempContenu = $contenu['index']['fr']['question 1']['contenu'];
+                    $tempId = $contenu['index']['fr']['question 1']['id'];
+                ?>
+                <?php include('repeat.php'); ?>
+            </div>
+            <div class="language">
+                <?php
+                    $tempContenu = $contenu['index']['en']['question 1']['contenu'];
+                    $tempId = $contenu['index']['en']['question 1']['id'];
+                ?>
+                <?php include('repeat.php'); ?>
+            </div>
+        </div>
+        <div class="content">
+            <div class="language">
+                <?php
+                    $tempContenu = $contenu['index']['fr']['question 2']['contenu'];
+                    $tempId = $contenu['index']['fr']['question 2']['id'];
+                ?>
+                <?php include('repeat.php'); ?>
+            </div>
+            <div class="language">
+                <?php
+                    $tempContenu = $contenu['index']['en']['question 2']['contenu'];
+                    $tempId = $contenu['index']['en']['question 2']['id'];
+                ?>
+                <?php include('repeat.php'); ?>
+            </div>
+        </div>
+        <div class="content">
+            <div class="language">
+                <?php
+                    $tempContenu = $contenu['index']['fr']['question 3']['contenu'];
+                    $tempId = $contenu['index']['fr']['question 3']['id'];
+                ?>
+                <?php include('repeat.php'); ?>
+            </div>
+            <div class="language">
+                <?php
+                    $tempContenu = $contenu['index']['en']['question 3']['contenu'];
+                    $tempId = $contenu['index']['en']['question 3']['id'];
+                ?>
+                <?php include('repeat.php'); ?>
+            </div>
+        </div>
+    <div class="group-content">
+
+
+
+
+    </div>
+        <div class="content">
+            <div class="language">
+                <?php
+                    $tempContenu = $contenu['index']['fr']['question 4']['contenu'];
+                    $tempId = $contenu['index']['fr']['question 4']['id'];
+                ?>
+                <?php include('repeat.php'); ?>
+            </div>
+            <div class="language">
+                <?php
+                    $tempContenu = $contenu['index']['en']['question 4']['contenu'];
+                    $tempId = $contenu['index']['en']['question 4']['id'];
+                ?>
+                <?php include('repeat.php'); ?>
+            </div>
+        </div>
+        <div class="content">
+            <div class="language">
+                <?php
+                    $tempContenu = $contenu['index']['fr']['question 5']['contenu'];
+                    $tempId = $contenu['index']['fr']['question 5']['id'];
+                ?>
+                <?php include('repeat.php'); ?>
+            </div>
+            <div class="language">
+                <?php
+                    $tempContenu = $contenu['index']['en']['question 5']['contenu'];
+                    $tempId = $contenu['index']['en']['question 5']['id'];
+                ?>
+                <?php include('repeat.php'); ?>
+            </div>
+        </div>
+        <div class="content">
+            <div class="language">
+                <?php
+                    $tempContenu = $contenu['index']['fr']['question 6']['contenu'];
+                    $tempId = $contenu['index']['fr']['question 6']['id'];
+                ?>
+                <?php include('repeat.php'); ?>
+            </div>
+            <div class="language">
+                <?php
+                    $tempContenu = $contenu['index']['en']['question 6']['contenu'];
+                    $tempId = $contenu['index']['en']['question 6']['id'];
+                ?>
+                <?php include('repeat.php'); ?>
+            </div>
+        </div>
+    </div>
+
+
+
+    
+    <div class="group-content">
+        <div class="content">
+            <div class="language">
+                <?php
+                    $tempContenu = $contenu['index']['fr']['valeurs 0']['contenu'];
+                    $tempId = $contenu['index']['fr']['valeurs 0']['id'];
+                ?>
+                <?php include('repeat.php'); ?>
+            </div>
+            <div class="language">
+                <?php
+                    $tempContenu = $contenu['index']['en']['valeurs 0']['contenu'];
+                    $tempId = $contenu['index']['en']['valeurs 0']['id'];
+                ?>
+                <?php include('repeat.php'); ?>
+            </div>
+        </div>
+        <div class="content">
+            <div class="language">
+                <?php
+                    $tempContenu = $contenu['index']['fr']['valeurs 1']['contenu'];
+                    $tempId = $contenu['index']['fr']['valeurs 1']['id'];
+                ?>
+                <?php include('repeat.php'); ?>
+            </div>
+            <div class="language">
+                <?php
+                    $tempContenu = $contenu['index']['en']['valeurs 1']['contenu'];
+                    $tempId = $contenu['index']['en']['valeurs 1']['id'];
+                ?>
+                <?php include('repeat.php'); ?>
+            </div>
+        </div>
+        <div class="content">
+            <div class="language">
+                <?php
+                    $tempContenu = $contenu['index']['fr']['valeurs 2']['contenu'];
+                    $tempId = $contenu['index']['fr']['valeurs 2']['id'];
+                ?>
+                <?php include('repeat.php'); ?>
+            </div>
+            <div class="language">
+                <?php
+                    $tempContenu = $contenu['index']['en']['valeurs 2']['contenu'];
+                    $tempId = $contenu['index']['en']['valeurs 2']['id'];
+                ?>
+                <?php include('repeat.php'); ?>
+            </div>
+        </div>
+        <div class="content">
+            <div class="language">
+                <?php
+                    $tempContenu = $contenu['index']['fr']['valeurs 3']['contenu'];
+                    $tempId = $contenu['index']['fr']['valeurs 3']['id'];
+                ?>
+                <?php include('repeat.php'); ?>
+            </div>
+            <div class="language">
+                <?php
+                    $tempContenu = $contenu['index']['en']['valeurs 3']['contenu'];
+                    $tempId = $contenu['index']['en']['valeurs 3']['id'];
+                ?>
+                <?php include('repeat.php'); ?>
+            </div>
+        </div>
+        <div class="content">
+            <div class="language">
+                <?php
+                    $tempContenu = $contenu['index']['fr']['valeurs 4']['contenu'];
+                    $tempId = $contenu['index']['fr']['valeurs 4']['id'];
+                ?>
+                <?php include('repeat.php'); ?>
+            </div>
+            <div class="language">
+                <?php
+                    $tempContenu = $contenu['index']['en']['valeurs 4']['contenu'];
+                    $tempId = $contenu['index']['en']['valeurs 4']['id'];
+                ?>
+                <?php include('repeat.php'); ?>
+            </div>
+        </div>
+        <div class="content">
+            <div class="language">
+                <?php
+                    $tempContenu = $contenu['index']['fr']['valeurs 5']['contenu'];
+                    $tempId = $contenu['index']['fr']['valeurs 5']['id'];
+                ?>
+                <?php include('repeat.php'); ?>
+            </div>
+            <div class="language">
+                <?php
+                    $tempContenu = $contenu['index']['en']['valeurs 5']['contenu'];
+                    $tempId = $contenu['index']['en']['valeurs 5']['id'];
+                ?>
+                <?php include('repeat.php'); ?>
+            </div>
+        </div>
+        <div class="content">
+            <div class="language">
+                <?php
+                    $tempContenu = $contenu['index']['fr']['valeurs 6']['contenu'];
+                    $tempId = $contenu['index']['fr']['valeurs 6']['id'];
+                ?>
+                <?php include('repeat.php'); ?>
+            </div>
+            <div class="language">
+                <?php
+                    $tempContenu = $contenu['index']['en']['valeurs 6']['contenu'];
+                    $tempId = $contenu['index']['en']['valeurs 6']['id'];
+                ?>
+                <?php include('repeat.php'); ?>
+            </div>
+        </div>
+        <div class="content">
+            <div class="language">
+            </div>
+            <div class="language">
+                <?php
+                    $tempContenu = $contenu['index']['en']['valeurs 7']['contenu'];
+                    $tempId = $contenu['index']['en']['valeurs 7']['id'];
+                ?>
+                <?php include('repeat.php'); ?>
+            </div>
+            
+        </div>
+    </div>
+</div>
