9a4f3d20c3f7c69d0526dd5ce3e88715a77d38f4
Enzobu <enzo.palermo@epsi.fr>
2023-06-12 16:23:28 +0200

    add class for active

--- admin/php/header.php
+++ admin/php/header.php
@@ -1,11 +1,11 @@
 <div class="nav">
     <a href="/fr/" class="img-logo"><img src="/img/logoV2.00.png" alt="logo Climat-2030"></a>
     <ul class="nav-list" id="nav">
-        <li class="nav-item"><a href="/admin/">Accueil</a></li>
-        <li class="nav-item"><a href="/admin/tables/users.php">Users</a></li>
-        <li class="nav-item"><a href="/admin/tables/customers.php">Customers</a></li>
-        <li class="nav-item"><a href="/admin/tables/contents.php">Contents</a></li>
-        <li class="nav-item"><a href="/admin/my-account.php">Mon compte</a></li>
+        <li class="nav-item accueil"><a href="/admin/">Accueil</a></li>
+        <li class="nav-item users"><a href="/admin/tables/users.php">Users</a></li>
+        <li class="nav-item customers"><a href="/admin/tables/customers.php">Customers</a></li>
+        <li class="nav-item contents"><a href="/admin/tables/contents.php">Contents</a></li>
+        <li class="nav-item my-account"><a href="/admin/my-account.php">Mon compte</a></li>
         <li class="nav-item"><a href="/admin/login.php?destroy="><img src="/admin/img/bouton-marche.svg" alt="Déconnection"></a></li>
     </ul>
 </div>
