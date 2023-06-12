002892a5427ad8c879420fc5a637ddd7a430ea4e
Enzobu <enzo.palermo@epsi.fr>
2023-06-12 16:26:22 +0200

    php for update contents and start fct to replace parts of code

--- admin/tables/contents.php
+++ admin/tables/contents.php
@@ -5,11 +5,94 @@
     $sql = "SELECT * FROM `contents` ORDER BY id_contents ASC";
     $requete = $db->query($sql);
     $contents = $requete->fetchAll();
-    // header("Refresh: 0;url=/admin/tables/contents.php");
 
-    // print_r($contents);
+    function content($donnees) {
+        $contenu = array();
+    
+        foreach ($donnees as $ligne) {
+            $page = $ligne['contents_page'];
+            $langue = $ligne['contents_language'];
+            $titre = $ligne['contents_name'];
+            $texte = $ligne['contents_content'];
+            $id = $ligne['id_contents'];
+    
+            if (!isset($contenu[$page])) {
+                $contenu[$page] = array();
+            }
+    
+            if (!isset($contenu[$page][$langue])) {
+                $contenu[$page][$langue] = array();
+            }
+    
+            if (!isset($contenu[$page][$langue][$titre])) {
+                $contenu[$page][$langue][$titre] = array();
+            }
+    
+            $contenu[$page][$langue][$titre] = array(
+                'contenu' => $texte,
+                'id' => $id
+            );
+        }
+    
+        return $contenu;
+    }
 
+    function contentView($donnees) {
+        $contenu = array();
+    
+        foreach ($donnees as $ligne) {
+            $page = $ligne['contents_page'];
+            $langue = $ligne['contents_language'];
+            $titre = $ligne['contents_name'];
+            $texte = $ligne['contents_content'];
+            $id = $ligne['id_contents'];
+    
+            if (!isset($contenu[$page])) {
+                $contenu[$page] = array();
+            }
+    
+            if (!isset($contenu[$page][$langue])) {
+                $contenu[$page][$langue] = array();
+            }
+    
+            if (!isset($contenu[$page][$langue][$titre])) {
+                $contenu[$page][$langue][$titre] = array();
+            }
+    
+            $contenu[$page][$langue][$titre] = array(
+                'contenu' => texteView($texte),
+                'id' => $id
+            );
+        }
+    
+        return $contenu;
+    }
 
+    function texteView($texte) {
+        $texte = str_replace('{{/', '</span ', $texte);
+        $texte = str_replace('{{', '<span class="', $texte);
+        $texte = str_replace('}}', '">', $texte);
+    
+        foreach ($replacements as $recherche => $remplacement) {
+            $texte = str_replace($recherche, $remplacement, $texte);
+        }
+        return $texte;
+    }
+
+    if(array_key_exists('action', $_GET) and
+    array_key_exists('id_contents', $_GET) and
+    array_key_exists('contents_content', $_GET)) {
+        $id_contents = htmlspecialchars($_GET['id_contents']);
+        $contents_content = htmlspecialchars($_GET['contents_content']);
+
+        $sql = "UPDATE `contents` SET `contents_content`=\"$contents_content\" WHERE id_contents = $id_contents";
+        $requete = $db->query($sql);
+
+        header("Refresh: 0;url=/admin/tables/contents.php");
+    }
+
+    $contenu = content($contents);
+    $contenuView = contentView($contents);
 ?>
 
 <!DOCTYPE html>
