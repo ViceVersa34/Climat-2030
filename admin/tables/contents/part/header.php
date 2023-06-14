cbe16082843bffb8e85f4e2bb5d8bb49d0b17cfe
Enzobu <enzo.palermo@epsi.fr>
2023-06-14 15:35:00 +0200

    create header manage text page

---
+++ admin/tables/contents/part/header.php
@@ -0,0 +1,263 @@
+<div class="page-selector" id="index"></div>
+<h2 class="title-page">Header</h2>
+<div class="contents">
+    <div class="group-content">
+        <div class="content">
+            <div class="language">
+                <?php 
+                    $tempContenu = $contenu['header']['fr']['page 1']['contenu'];
+                    $tempContenuView = $contenuView['header']['fr']['page 1']['contenu'];
+                    $tempId = $contenu['header']['fr']['page 1']['id'];
+                    $language = 'fr';
+                ?>
+                <?php include('repeat.php'); ?>
+            </div>
+            <div class="language">
+            <?php 
+                    $tempContenu = $contenu['header']['en']['page 1']['contenu'];
+                    $tempContenuView = $contenuView['header']['en']['page 1']['contenu'];
+                    $tempId = $contenu['header']['en']['page 1']['id'];
+                    $language = 'en';
+                ?>
+                <?php include('repeat.php'); ?>
+            </div>            
+        </div>
+    </div>
+
+    <div class="group-content">
+        <div class="content">
+            <div class="language">
+                <?php 
+                    $tempContenu = $contenu['header']['fr']['page 2']['contenu'];
+                    $tempContenuView = $contenuView['header']['fr']['page 2']['contenu'];
+                    $tempId = $contenu['header']['fr']['page 2']['id']; 
+                    $language = 'fr';
+                ?>
+                <?php include('repeat.php'); ?>
+                <?php 
+                    $tempContenu = $contenu['header']['fr']['page 2.1']['contenu'];
+                    $tempContenuView = $contenuView['header']['fr']['page 2.1']['contenu'];
+                    $tempId = $contenu['header']['fr']['page 2.1']['id']; 
+                    $language = 'fr';
+                ?>
+                <?php include('repeat.php'); ?>
+                <?php 
+                    $tempContenu = $contenu['header']['fr']['page 2.2']['contenu'];
+                    $tempContenuView = $contenuView['header']['fr']['page 2.2']['contenu'];
+                    $tempId = $contenu['header']['fr']['page 2.2']['id']; 
+                    $language = 'fr';
+                ?>
+                <?php include('repeat.php'); ?>
+                <?php 
+                    $tempContenu = $contenu['header']['fr']['page 2.3']['contenu'];
+                    $tempContenuView = $contenuView['header']['fr']['page 2.3']['contenu'];
+                    $tempId = $contenu['header']['fr']['page 2.3']['id']; 
+                    $language = 'fr';
+                ?>
+                <?php include('repeat.php'); ?>
+                <?php 
+                    $tempContenu = $contenu['header']['fr']['page 2.4']['contenu'];
+                    $tempContenuView = $contenuView['header']['fr']['page 2.4']['contenu'];
+                    $tempId = $contenu['header']['fr']['page 2.4']['id']; 
+                    $language = 'fr';
+                ?>
+                <?php include('repeat.php'); ?>
+            </div>
+            <div class="language">
+            <?php 
+                    $tempContenu = $contenu['header']['en']['page 2']['contenu'];
+                    $tempContenuView = $contenuView['header']['en']['page 2']['contenu'];
+                    $tempId = $contenu['header']['en']['page 2']['id']; 
+                    $language = 'en';
+                ?>
+                <?php include('repeat.php'); ?>
+                <?php 
+                    $tempContenu = $contenu['header']['en']['page 2.1']['contenu'];
+                    $tempContenuView = $contenuView['header']['en']['page 2.1']['contenu'];
+                    $tempId = $contenu['header']['en']['page 2.1']['id']; 
+                    $language = 'en';
+                ?>
+                <?php include('repeat.php'); ?>
+                <?php 
+                    $tempContenu = $contenu['header']['en']['page 2.2']['contenu'];
+                    $tempContenuView = $contenuView['header']['en']['page 2.2']['contenu'];
+                    $tempId = $contenu['header']['en']['page 2.2']['id']; 
+                    $language = 'en';
+                ?>
+                <?php include('repeat.php'); ?>
+                <?php 
+                    $tempContenu = $contenu['header']['en']['page 2.3']['contenu'];
+                    $tempContenuView = $contenuView['header']['en']['page 2.3']['contenu'];
+                    $tempId = $contenu['header']['en']['page 2.3']['id']; 
+                    $language = 'en';
+                ?>
+                <?php include('repeat.php'); ?>
+                <?php 
+                    $tempContenu = $contenu['header']['en']['page 2.4']['contenu'];
+                    $tempContenuView = $contenuView['header']['en']['page 2.4']['contenu'];
+                    $tempId = $contenu['header']['en']['page 2.4']['id']; 
+                    $language = 'en';
+                ?>
+                <?php include('repeat.php'); ?>
+            </div>            
+        </div>
+    </div>
+
+    <div class="group-content">
+        <div class="content">
+            <div class="language">
+                <?php 
+                    $tempContenu = $contenu['header']['fr']['page 3']['contenu'];
+                    $tempContenuView = $contenuView['header']['fr']['page 3']['contenu'];
+                    $tempId = $contenu['header']['fr']['page 3']['id']; 
+                    $language = 'fr';
+                ?>
+                <?php include('repeat.php'); ?>
+                <?php 
+                    $tempContenu = $contenu['header']['fr']['page 3.1']['contenu'];
+                    $tempContenuView = $contenuView['header']['fr']['page 3.1']['contenu'];
+                    $tempId = $contenu['header']['fr']['page 3.1']['id']; 
+                    $language = 'fr';
+                ?>
+                <?php include('repeat.php'); ?>
+                <?php 
+                    $tempContenu = $contenu['header']['fr']['page 3.2']['contenu'];
+                    $tempContenuView = $contenuView['header']['fr']['page 3.2']['contenu'];
+                    $tempId = $contenu['header']['fr']['page 3.2']['id']; 
+                    $language = 'fr';
+                ?>
+                <?php include('repeat.php'); ?>
+                <?php 
+                    $tempContenu = $contenu['header']['fr']['page 3.3']['contenu'];
+                    $tempContenuView = $contenuView['header']['fr']['page 3.3']['contenu'];
+                    $tempId = $contenu['header']['fr']['page 3.3']['id']; 
+                    $language = 'fr';
+                ?>
+                <?php include('repeat.php'); ?>
+                <?php 
+                    $tempContenu = $contenu['header']['fr']['page 3.4']['contenu'];
+                    $tempContenuView = $contenuView['header']['fr']['page 3.4']['contenu'];
+                    $tempId = $contenu['header']['fr']['page 3.4']['id']; 
+                    $language = 'fr';
+                ?>
+                <?php include('repeat.php'); ?>
+            </div>
+            <div class="language">
+            <?php 
+                    $tempContenu = $contenu['header']['en']['page 3']['contenu'];
+                    $tempContenuView = $contenuView['header']['en']['page 3']['contenu'];
+                    $tempId = $contenu['header']['en']['page 3']['id']; 
+                    $language = 'en';
+                ?>
+                <?php include('repeat.php'); ?>
+                <?php 
+                    $tempContenu = $contenu['header']['en']['page 3.1']['contenu'];
+                    $tempContenuView = $contenuView['header']['en']['page 3.1']['contenu'];
+                    $tempId = $contenu['header']['en']['page 3.1']['id']; 
+                    $language = 'en';
+                ?>
+                <?php include('repeat.php'); ?>
+                <?php 
+                    $tempContenu = $contenu['header']['en']['page 3.2']['contenu'];
+                    $tempContenuView = $contenuView['header']['en']['page 3.2']['contenu'];
+                    $tempId = $contenu['header']['en']['page 3.2']['id']; 
+                    $language = 'en';
+                ?>
+                <?php include('repeat.php'); ?>
+                <?php 
+                    $tempContenu = $contenu['header']['en']['page 3.3']['contenu'];
+                    $tempContenuView = $contenuView['header']['en']['page 3.3']['contenu'];
+                    $tempId = $contenu['header']['en']['page 3.3']['id']; 
+                    $language = 'en';
+                ?>
+                <?php include('repeat.php'); ?>
+                <?php 
+                    $tempContenu = $contenu['header']['en']['page 3.4']['contenu'];
+                    $tempContenuView = $contenuView['header']['en']['page 3.4']['contenu'];
+                    $tempId = $contenu['header']['en']['page 3.4']['id']; 
+                    $language = 'en';
+                ?>
+                <?php include('repeat.php'); ?>
+            </div>            
+        </div>
+    </div>
+
+    <div class="group-content">
+        <div class="content">
+            <div class="language">
+                <?php 
+                    $tempContenu = $contenu['header']['fr']['page 4']['contenu'];
+                    $tempContenuView = $contenuView['header']['fr']['page 4']['contenu'];
+                    $tempId = $contenu['header']['fr']['page 4']['id']; 
+                    $language = 'fr';
+                ?>
+                <?php include('repeat.php'); ?>
+                <?php 
+                    $tempContenu = $contenu['header']['fr']['page 4.1']['contenu'];
+                    $tempContenuView = $contenuView['header']['fr']['page 4.1']['contenu'];
+                    $tempId = $contenu['header']['fr']['page 4.1']['id']; 
+                    $language = 'fr';
+                ?>
+                <?php include('repeat.php'); ?>
+                <?php 
+                    $tempContenu = $contenu['header']['fr']['page 4.2']['contenu'];
+                    $tempContenuView = $contenuView['header']['fr']['page 4.2']['contenu'];
+                    $tempId = $contenu['header']['fr']['page 4.2']['id']; 
+                    $language = 'fr';
+                ?>
+                <?php include('repeat.php'); ?>
+                <?php 
+                    $tempContenu = $contenu['header']['fr']['page 4.3']['contenu'];
+                    $tempContenuView = $contenuView['header']['fr']['page 4.3']['contenu'];
+                    $tempId = $contenu['header']['fr']['page 4.3']['id']; 
+                    $language = 'fr';
+                ?>
+                <?php include('repeat.php'); ?>
+                <?php 
+                    $tempContenu = $contenu['header']['fr']['page 4.4']['contenu'];
+                    $tempContenuView = $contenuView['header']['fr']['page 4.4']['contenu'];
+                    $tempId = $contenu['header']['fr']['page 4.4']['id']; 
+                    $language = 'fr';
+                ?>
+                <?php include('repeat.php'); ?>
+            </div>
+            <div class="language">
+                <?php 
+                    $tempContenu = $contenu['header']['en']['page 4']['contenu'];
+                    $tempContenuView = $contenuView['header']['en']['page 4']['contenu'];
+                    $tempId = $contenu['header']['en']['page 4']['id']; 
+                    $language = 'en';
+                ?>
+                <?php include('repeat.php'); ?>
+                <?php 
+                    $tempContenu = $contenu['header']['en']['page 4.1']['contenu'];
+                    $tempContenuView = $contenuView['header']['en']['page 4.1']['contenu'];
+                    $tempId = $contenu['header']['en']['page 4.1']['id']; 
+                    $language = 'en';
+                ?>
+                <?php include('repeat.php'); ?>
+                <?php 
+                    $tempContenu = $contenu['header']['en']['page 4.2']['contenu'];
+                    $tempContenuView = $contenuView['header']['en']['page 4.2']['contenu'];
+                    $tempId = $contenu['header']['en']['page 4.2']['id']; 
+                    $language = 'en';
+                ?>
+                <?php include('repeat.php'); ?>
+                <?php 
+                    $tempContenu = $contenu['header']['en']['page 4.3']['contenu'];
+                    $tempContenuView = $contenuView['header']['en']['page 4.3']['contenu'];
+                    $tempId = $contenu['header']['en']['page 4.3']['id']; 
+                    $language = 'en';
+                ?>
+                <?php include('repeat.php'); ?>
+                <?php 
+                    $tempContenu = $contenu['header']['en']['page 4.4']['contenu'];
+                    $tempContenuView = $contenuView['header']['en']['page 4.4']['contenu'];
+                    $tempId = $contenu['header']['en']['page 4.4']['id']; 
+                    $language = 'en';
+                ?>
+                <?php include('repeat.php'); ?>
+            </div>            
+        </div>
+    </div>
+</div>
