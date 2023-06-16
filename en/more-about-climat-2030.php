58123413e9da1b74e69527f6abf8062ad3e7bfe4
Enzobu <enzo.palermo@epsi.fr>
2023-06-16 12:00:41 +0200

    Update more-about-climat-2030.php

--- en/more-about-climat-2030.php
+++ en/more-about-climat-2030.php
@@ -13,76 +13,34 @@
 
     <main>
 
-        <h1 class="top_txt">More about <span class="orange-txt size-txt2 txt-g font-family">Climat-2030</span></h1>
+        <h1 class="top_txt"><?php echo html_entity_decode($contenuView['en-savoir-plus']['en']['titre']['contenu']); ?></h1>
 
         <div class="content">
             <div class="manifeste">
-                <h2 class="yello-txt">Manifesto : We are in danger.</h2>
-                <p>
-                    Scientists prove every day that we have been producing greenhouse gases for nearly 200 years at an exceptional 
-                    rate compared to centuries ago. Those fossil fuels no longer dissipate in the atmosphere because there are too 
-                    many and continue to be added, which is why there is a warming climate around the world. <br><br>
-
-                    The alert was issued as early as 1896, but it is only now that we are beginning to see the effects. Companies 
-                    still continue to produce more and more greenhouse gases every day with no legal action by the world’s politics 
-                    to ensure our safety and health. <br><br>
-
-                    Companies and countries have one thing in common: 82% of them are run by men of a certain age who decide 
-                    universally. Choices and decisions are de facto biased without the advice, counsel and consent of the other 
-                    half of humanity, women.<br><br>
-
-                    Imagine if, in past centuries, women but also young people and all those who feel being a minority, had been 
-                    so much decision-makers and responsible for our common good, the Earth? <br>
-                    History would not have been the same. Our survival might not be in danger today. <br><br>
-
-                    We all need to metamorphosis into climate virtuoso for humanity to survive beyond 2030. We need women, young 
-                    people, those who think being a minority, to have the audacity to play the game if we want to win.  <br><br>
-
-                    Hubert Reeves once said: «Man leads a fight against Nature, if he wins, he loses».
-                </p>
-                <div class="sign yello-txt">More about Estelle Couderc-Doat, founder of Climat-2030</div>
+                <h2><?php echo html_entity_decode($contenuView['en-savoir-plus']['en']['manifeste titre']['contenu']); ?></h2>
+                <p><?php echo html_entity_decode($contenuView['en-savoir-plus']['en']['manifeste']['contenu']); ?></p>
+                <div class="sign"><?php echo html_entity_decode($contenuView['en-savoir-plus']['en']['manifeste signature']['contenu']); ?></div>
             </div>
             <div class="raisons">
                 <div class="raison">
-                    <h3 class="yello-txt size-txt font-weight">Purpose.</h3>
-                    <p>
-                        I created Climat-2030 to stop the bleeding, to avoid the 16 irreversible tipping points from being reached. 
-                        I would like to participate in organizing a new sustainable society which respect for Living comes before 
-                        individual freedom.
-                    </p>
+                    <h3><?php echo html_entity_decode($contenuView['en-savoir-plus']['en']['titre raison 1']['contenu']); ?></h3>
+                    <p><?php echo html_entity_decode($contenuView['en-savoir-plus']['en']['raison 1']['contenu']); ?></p>
                 </div>
                 <div class="raison">
-                    <h3 class="yello-txt size-txt font-weight">What made me started.</h3>
-                    <p>
-                        On the one hand, the 2030 deadline and beyond to maintain 1.5°C of global warming, on the other hand the 
-                        eco anxiety of my son. 
-                    </p>
+                    <h3><?php echo html_entity_decode($contenuView['en-savoir-plus']['en']['titre raison 2']['contenu']); ?></h3>
+                    <p><?php echo html_entity_decode($contenuView['en-savoir-plus']['en']['raison 2']['contenu']); ?></p>
                 </div>
                 <div class="raison">
-                    <h3 class="yello-txt size-txt font-weight">What made me want to help others and provide the services I offer.</h3>
-                    <p>
-                        Empowering women means helping to limit global warming and the destruction of the Living because women’s 
-                        modus vivendi relies on education and ability to organize the change. <br>
-                        At the same time, deconstructing patriarchy means building a society without toxic virility that is costly 
-                        for humanity and carbon. Women must co-lead the world.
-                    </p>
+                    <h3><?php echo html_entity_decode($contenuView['en-savoir-plus']['en']['titre raison 3']['contenu']); ?></h3>
+                    <p><?php echo html_entity_decode($contenuView['en-savoir-plus']['en']['raison 3']['contenu']); ?></p>
                 </div>
                 <div class="raison">
-                    <h3 class="yello-txt size-txt font-weight">Why I keep moving forward.</h3>
-                    <p>
-                        Every day the bad news rains, our survival on Earth is counted and we continue to live as before the pandemic 
-                        when we saw during the pandemic that another way of life is possible, that everything can change and in just 
-                        a few days ! <br>
-                        When we realize that we are going straight to the wall, we have no choice but to act.  <span class="yello-txt">
-                            We must educate ourselves and then act.</span>.
-                    </p>
+                    <h3><?php echo html_entity_decode($contenuView['en-savoir-plus']['en']['titre raison 4']['contenu']); ?></h3>
+                    <p><?php echo html_entity_decode($contenuView['en-savoir-plus']['en']['raison 4']['contenu']); ?></p>
                 </div>
                 <div class="raison">
-                    <h3 class="yello-txt size-txt font-weight">Do I feel that my purpose guides me ?</h3>
-                    <p>
-                        Yes. I feel compelled. We must become collectively aware of the major problem of the 21st Century in order to 
-                        act to change the situation and change our behaviour habits. Individually and collectively.
-                    </p>
+                    <h3><?php echo html_entity_decode($contenuView['en-savoir-plus']['en']['titre raison 5']['contenu']); ?></h3>
+                    <p><?php echo html_entity_decode($contenuView['en-savoir-plus']['en']['raison 5']['contenu']); ?></p>
                 </div>
             </div>
         </div>
