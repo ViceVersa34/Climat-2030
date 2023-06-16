2bb984aca8349a3d6a6e2a2518a31bb2669b3db4
Enzobu <enzo.palermo@epsi.fr>
2023-06-16 12:00:44 +0200

    Update en-savoir-plus.php

--- fr/en-savoir-plus.php
+++ fr/en-savoir-plus.php
@@ -13,78 +13,34 @@
 
     <main>
 
-        <h1 class="top_txt">En savoir plus sur <span class="orange-txt size-txt2 txt-g font-family">Climat-2030</span></h1>
+        <h1 class="top_txt"><?php echo html_entity_decode($contenuView['en-savoir-plus']['fr']['titre']['contenu']); ?></h1>
 
         <div class="content">
             <div class="manifeste">
-                <h2 class="yello-txt">Manifeste : Nous sommes en danger.</h2>
-                <p>
-                Les scientifiques nous prouvent tous les jours que nous produisons depuis près de 200 ans des gaz à effet 
-                de serre à un rythme exceptionnel par rapport aux siècles passés. Ces GAS ne se dissipent plus dans l'atmosphère 
-                car il sont trop nombreux et continuent de s'ajouter; voilà pourquoi il y a un dérèglement climatique de plus 
-                en plus important partout dans le monde. <br><br>
-
-                L'alerte a été donné dès 1896 mais ce n'est que maintenant que nous commençons à en voir les effets. Pourtant, 
-                les entreprises continuent d'en produire encore davantage tous les jours sans que les politiques du monde ne 
-                prennent des mesures légales pour assurer notre sécurité et notre santé. <br><br>
-
-                Les entreprises et les pays ont une particularité en commun : Ils sont dirigés à 82% par des hommes d'un âge 
-                certain certain qui décident universellement. Les choix et les décisions sont de facto biaisés sans l'avis, 
-                les conseils et le consentement de l'autre moitié de l'humanité, les femmes. <br><br>
-
-                Imaginez si par les siècles passés, les femmes mais aussi les jeunes, ceux qui se sentent en minorité, avaient 
-                été autant décisionnaires et responsables de notre bien commun, la Terre ? <br>
-                L'Histoire n'aurait pas été la même. Notre survie ne serait peut être pas aujourd'hui en danger. <br><br>
-
-                Nous devons tous nous métamorphoser en virtuose du climat pour que l'Humanité subsiste après 2030. 
-                Nous avons besoin que les femmes, les jeunes, ceux qui se pensent en minorité aient l'audace de rentrer dans 
-                le jeu si nous voulons gagner. <br><br>
-
-                Hubert Reeves a dit : « L'homme mène un combat contre la Nature, s'il gagne, il perd ».
-                </p>
-                <div class="sign yello-txt  ">Estelle Couderc-Doat, fondatrice de Climat-2030</div>
+                <h2><?php echo html_entity_decode($contenuView['en-savoir-plus']['fr']['manifeste titre']['contenu']); ?></h2>
+                <p><?php echo html_entity_decode($contenuView['en-savoir-plus']['fr']['manifeste']['contenu']); ?></p>
+                <div class="sign"><?php echo html_entity_decode($contenuView['en-savoir-plus']['fr']['manifeste signature']['contenu']); ?></div>
             </div>
             <div class="raisons">
                 <div class="raison">
-                    <h3 class="yello-txt size-txt font-weight">Raison d'être.</h3>
-                    <p>
-                        J'ai créé Climat-2030 pour participer à stopper l'hémorragie, pour éviter que les 16 points de bascules 
-                        irréversibles ne soient atteints. Je voudrais participer à organiser une nouvelle société viable où le 
-                        respect du vivant passe avant toute liberté individuelle.
-                    </p>
+                    <h3><?php echo html_entity_decode($contenuView['en-savoir-plus']['fr']['titre raison 1']['contenu']); ?></h3>
+                    <p><?php echo html_entity_decode($contenuView['en-savoir-plus']['fr']['raison 1']['contenu']); ?></p>
                 </div>
                 <div class="raison">
-                    <h3 class="yello-txt size-txt font-weight">Ce qui m'a fait commencer.</h3>
-                    <p>
-                        D'un part, la deadline de 2030 et au delà pour maintenir 1,5°C de réchauffement climatique, d'autre part 
-                        l'éco anxiété de mon fils. 
-                    </p>
+                    <h3><?php echo html_entity_decode($contenuView['en-savoir-plus']['fr']['titre raison 2']['contenu']); ?></h3>
+                    <p><?php echo html_entity_decode($contenuView['en-savoir-plus']['fr']['raison 2']['contenu']); ?></p>
                 </div>
                 <div class="raison">
-                    <h3 class="yello-txt size-txt font-weight">Ce qui m'a donné envie d'aider les autres et de fournir les services que je propose.</h3>
-                    <p>
-                        Donner du pouvoir aux femmes, c'est participer à limiter le réchauffement climatique et la destruction du 
-                        Vivant car le modus vivendi des femmes repose sur l'éducation et leur habileté à organiser le changement. <br>
-                        En parallèle, déconstruire le patriarcat, c'est construire une société sans virilité toxique coûteuse pour 
-                        l'humanité et en carbone. Les femmes doivent co-conduire le monde.
-                    </p>
+                    <h3><?php echo html_entity_decode($contenuView['en-savoir-plus']['fr']['titre raison 3']['contenu']); ?></h3>
+                    <p><?php echo html_entity_decode($contenuView['en-savoir-plus']['fr']['raison 3']['contenu']); ?></p>
                 </div>
                 <div class="raison">
-                    <h3 class="yello-txt size-txt font-weight">Pourquoi je  continue à aller de l'avant.</h3>
-                    <p>
-                        Tous les jours les mauvaises nouvelles pleuvent, notre survie sur Terre est comptée et l'on continue de vivre 
-                        comme avant la pandémie alors qu'on a vu pendant la pandémie qu'une autre vie est possible, que tout peut 
-                        changer et en quelques jours à peine ! <br>
-                        Quand on se rend compte qu'on va droit dans le mur, on a pas le choix, il faut agir. <span class="yello-txt">S'éduquer d'abord et 
-                        agir ensuite</span>.
-                    </p>
+                    <h3><?php echo html_entity_decode($contenuView['en-savoir-plus']['fr']['titre raison 4']['contenu']); ?></h3>
+                    <p><?php echo html_entity_decode($contenuView['en-savoir-plus']['fr']['raison 4']['contenu']); ?></p>
                 </div>
                 <div class="raison">
-                    <h3 class="yello-txt size-txt font-weight">Ai-je le sentiment que ma raison d'être me guide ?</h3>
-                    <p>
-                        Oui. Je me sens obligée. Il faut prendre conscience collectivement du problème majeur du 21° Siècle de façon à 
-                        agir pour changer la donne et changer nos habitudes de comportement. Individuellement et collectivement.
-                    </p>
+                    <h3><?php echo html_entity_decode($contenuView['en-savoir-plus']['fr']['titre raison 5']['contenu']); ?></h3>
+                    <p><?php echo html_entity_decode($contenuView['en-savoir-plus']['fr']['raison 5']['contenu']); ?></p>
                 </div>
             </div>
         </div>
