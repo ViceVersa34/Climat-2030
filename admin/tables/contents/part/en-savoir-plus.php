c1c0fcaf6dd4985a619e6482c72152bceb4e632b
Enzobu <enzo.palermo@epsi.fr>
2023-06-16 12:00:37 +0200

    Create en-savoir-plus.php

---
+++ admin/tables/contents/part/en-savoir-plus.php
@@ -0,0 +1,253 @@
+<div class="page-selector" id="CGU"></div>
+<h2 class="title-page">En savoir plus</h2>
+<div class="contents">
+    <div class="group-content">
+            <div class="language">
+                <?php
+                    $tempContenu = $contenu['en-savoir-plus']['fr']['titre']['contenu'];
+                    $tempContenuView = $contenuView['en-savoir-plus']['fr']['titre']['contenu'];
+                    $tempId = $contenu['en-savoir-plus']['fr']['titre']['id'];
+                    $language = 'fr';
+                ?>
+                <?php include('repeat.php'); ?>
+            </div>
+            <div class="language">
+                <?php
+                    $tempContenu = $contenu['en-savoir-plus']['en']['titre']['contenu'];
+                    $tempContenuView = $contenuView['en-savoir-plus']['en']['titre']['contenu'];
+                    $tempId = $contenu['en-savoir-plus']['en']['titre']['id'];
+                    $language = 'en';
+                ?>
+                <?php include('repeat.php'); ?>
+            </div>
+        </div>
+    </div>
+    
+    <div class="group-content">
+            <div class="language">
+                <?php
+                    $tempContenu = $contenu['en-savoir-plus']['fr']['manifeste titre']['contenu'];
+                    $tempContenuView = $contenuView['en-savoir-plus']['fr']['manifeste titre']['contenu'];
+                    $tempId = $contenu['en-savoir-plus']['fr']['manifeste titre']['id'];
+                    $language = 'fr';
+                ?>
+                <?php include('repeat.php'); ?>
+                <?php
+                    $tempContenu = $contenu['en-savoir-plus']['fr']['manifeste']['contenu'];
+                    $tempContenuView = $contenuView['en-savoir-plus']['fr']['manifeste']['contenu'];
+                    $tempId = $contenu['en-savoir-plus']['fr']['manifeste']['id'];
+                    $language = 'fr';
+                ?>
+                <?php include('repeat.php'); ?>
+                <?php
+                    $tempContenu = $contenu['en-savoir-plus']['fr']['manifeste signature']['contenu'];
+                    $tempContenuView = $contenuView['en-savoir-plus']['fr']['manifeste signature']['contenu'];
+                    $tempId = $contenu['en-savoir-plus']['fr']['manifeste signature']['id'];
+                    $language = 'fr';
+                ?>
+                <?php include('repeat.php'); ?>
+            </div>
+            <div class="language">
+                <?php
+                    $tempContenu = $contenu['en-savoir-plus']['en']['manifeste titre']['contenu'];
+                    $tempContenuView = $contenuView['en-savoir-plus']['en']['manifeste titre']['contenu'];
+                    $tempId = $contenu['en-savoir-plus']['en']['manifeste titre']['id'];
+                    $language = 'en';
+                ?>
+                <?php include('repeat.php'); ?>
+                <?php
+                    $tempContenu = $contenu['en-savoir-plus']['en']['manifeste']['contenu'];
+                    $tempContenuView = $contenuView['en-savoir-plus']['en']['manifeste']['contenu'];
+                    $tempId = $contenu['en-savoir-plus']['en']['manifeste']['id'];
+                    $language = 'en';
+                ?>
+                <?php include('repeat.php'); ?>
+                <?php
+                    $tempContenu = $contenu['en-savoir-plus']['en']['manifeste signature']['contenu'];
+                    $tempContenuView = $contenuView['en-savoir-plus']['en']['manifeste signature']['contenu'];
+                    $tempId = $contenu['en-savoir-plus']['en']['manifeste signature']['id'];
+                    $language = 'en';
+                ?>
+                <?php include('repeat.php'); ?>
+            </div>
+        </div>
+    </div>
+    
+    <div class="group-content">
+            <div class="language">
+                <?php
+                    $tempContenu = $contenu['en-savoir-plus']['fr']['titre raison 1']['contenu'];
+                    $tempContenuView = $contenuView['en-savoir-plus']['fr']['titre raison 1']['contenu'];
+                    $tempId = $contenu['en-savoir-plus']['fr']['titre raison 1']['id'];
+                    $language = 'fr';
+                ?>
+                <?php include('repeat.php'); ?>
+                <?php
+                    $tempContenu = $contenu['en-savoir-plus']['fr']['raison 1']['contenu'];
+                    $tempContenuView = $contenuView['en-savoir-plus']['fr']['raison 1']['contenu'];
+                    $tempId = $contenu['en-savoir-plus']['fr']['raison 1']['id'];
+                    $language = 'fr';
+                ?>
+                <?php include('repeat.php'); ?>
+            </div>
+            <div class="language">
+                <?php
+                    $tempContenu = $contenu['en-savoir-plus']['en']['titre raison 1']['contenu'];
+                    $tempContenuView = $contenuView['en-savoir-plus']['en']['titre raison 1']['contenu'];
+                    $tempId = $contenu['en-savoir-plus']['en']['titre raison 1']['id'];
+                    $language = 'en';
+                ?>
+                <?php include('repeat.php'); ?>
+                <?php
+                    $tempContenu = $contenu['en-savoir-plus']['en']['raison 1']['contenu'];
+                    $tempContenuView = $contenuView['en-savoir-plus']['en']['raison 1']['contenu'];
+                    $tempId = $contenu['en-savoir-plus']['en']['raison 1']['id'];
+                    $language = 'en';
+                ?>
+                <?php include('repeat.php'); ?>
+            </div>
+        </div>
+    </div>
+    <div class="group-content">
+            <div class="language">
+                <?php
+                    $tempContenu = $contenu['en-savoir-plus']['fr']['titre raison 2']['contenu'];
+                    $tempContenuView = $contenuView['en-savoir-plus']['fr']['titre raison 2']['contenu'];
+                    $tempId = $contenu['en-savoir-plus']['fr']['titre raison 2']['id'];
+                    $language = 'fr';
+                ?>
+                <?php include('repeat.php'); ?>
+                <?php
+                    $tempContenu = $contenu['en-savoir-plus']['fr']['raison 2']['contenu'];
+                    $tempContenuView = $contenuView['en-savoir-plus']['fr']['raison 2']['contenu'];
+                    $tempId = $contenu['en-savoir-plus']['fr']['raison 2']['id'];
+                    $language = 'fr';
+                ?>
+                <?php include('repeat.php'); ?>
+            </div>
+            <div class="language">
+                <?php
+                    $tempContenu = $contenu['en-savoir-plus']['en']['titre raison 2']['contenu'];
+                    $tempContenuView = $contenuView['en-savoir-plus']['en']['titre raison 2']['contenu'];
+                    $tempId = $contenu['en-savoir-plus']['en']['titre raison 2']['id'];
+                    $language = 'en';
+                ?>
+                <?php include('repeat.php'); ?>
+                <?php
+                    $tempContenu = $contenu['en-savoir-plus']['en']['raison 2']['contenu'];
+                    $tempContenuView = $contenuView['en-savoir-plus']['en']['raison 2']['contenu'];
+                    $tempId = $contenu['en-savoir-plus']['en']['raison 2']['id'];
+                    $language = 'en';
+                ?>
+                <?php include('repeat.php'); ?>
+            </div>
+        </div>
+    </div>
+    <div class="group-content">
+            <div class="language">
+                <?php
+                    $tempContenu = $contenu['en-savoir-plus']['fr']['titre raison 3']['contenu'];
+                    $tempContenuView = $contenuView['en-savoir-plus']['fr']['titre raison 3']['contenu'];
+                    $tempId = $contenu['en-savoir-plus']['fr']['titre raison 3']['id'];
+                    $language = 'fr';
+                ?>
+                <?php include('repeat.php'); ?>
+                <?php
+                    $tempContenu = $contenu['en-savoir-plus']['fr']['raison 3']['contenu'];
+                    $tempContenuView = $contenuView['en-savoir-plus']['fr']['raison 3']['contenu'];
+                    $tempId = $contenu['en-savoir-plus']['fr']['raison 3']['id'];
+                    $language = 'fr';
+                ?>
+                <?php include('repeat.php'); ?>
+            </div>
+            <div class="language">
+                <?php
+                    $tempContenu = $contenu['en-savoir-plus']['en']['titre raison 3']['contenu'];
+                    $tempContenuView = $contenuView['en-savoir-plus']['en']['titre raison 3']['contenu'];
+                    $tempId = $contenu['en-savoir-plus']['en']['titre raison 3']['id'];
+                    $language = 'en';
+                ?>
+                <?php include('repeat.php'); ?>
+                <?php
+                    $tempContenu = $contenu['en-savoir-plus']['en']['raison 3']['contenu'];
+                    $tempContenuView = $contenuView['en-savoir-plus']['en']['raison 3']['contenu'];
+                    $tempId = $contenu['en-savoir-plus']['en']['raison 3']['id'];
+                    $language = 'en';
+                ?>
+                <?php include('repeat.php'); ?>
+            </div>
+        </div>
+    </div>
+    <div class="group-content">
+            <div class="language">
+                <?php
+                    $tempContenu = $contenu['en-savoir-plus']['fr']['titre raison 4']['contenu'];
+                    $tempContenuView = $contenuView['en-savoir-plus']['fr']['titre raison 4']['contenu'];
+                    $tempId = $contenu['en-savoir-plus']['fr']['titre raison 4']['id'];
+                    $language = 'fr';
+                ?>
+                <?php include('repeat.php'); ?>
+                <?php
+                    $tempContenu = $contenu['en-savoir-plus']['fr']['raison 4']['contenu'];
+                    $tempContenuView = $contenuView['en-savoir-plus']['fr']['raison 4']['contenu'];
+                    $tempId = $contenu['en-savoir-plus']['fr']['raison 4']['id'];
+                    $language = 'fr';
+                ?>
+                <?php include('repeat.php'); ?>
+            </div>
+            <div class="language">
+                <?php
+                    $tempContenu = $contenu['en-savoir-plus']['en']['titre raison 4']['contenu'];
+                    $tempContenuView = $contenuView['en-savoir-plus']['en']['titre raison 4']['contenu'];
+                    $tempId = $contenu['en-savoir-plus']['en']['titre raison 4']['id'];
+                    $language = 'en';
+                ?>
+                <?php include('repeat.php'); ?>
+                <?php
+                    $tempContenu = $contenu['en-savoir-plus']['en']['raison 4']['contenu'];
+                    $tempContenuView = $contenuView['en-savoir-plus']['en']['raison 4']['contenu'];
+                    $tempId = $contenu['en-savoir-plus']['en']['raison 4']['id'];
+                    $language = 'en';
+                ?>
+                <?php include('repeat.php'); ?>
+            </div>
+        </div>
+    </div>
+    <div class="group-content">
+            <div class="language">
+                <?php
+                    $tempContenu = $contenu['en-savoir-plus']['fr']['titre raison 5']['contenu'];
+                    $tempContenuView = $contenuView['en-savoir-plus']['fr']['titre raison 5']['contenu'];
+                    $tempId = $contenu['en-savoir-plus']['fr']['titre raison 5']['id'];
+                    $language = 'fr';
+                ?>
+                <?php include('repeat.php'); ?>
+                <?php
+                    $tempContenu = $contenu['en-savoir-plus']['fr']['raison 5']['contenu'];
+                    $tempContenuView = $contenuView['en-savoir-plus']['fr']['raison 5']['contenu'];
+                    $tempId = $contenu['en-savoir-plus']['fr']['raison 5']['id'];
+                    $language = 'fr';
+                ?>
+                <?php include('repeat.php'); ?>
+            </div>
+            <div class="language">
+                <?php
+                    $tempContenu = $contenu['en-savoir-plus']['en']['titre raison 5']['contenu'];
+                    $tempContenuView = $contenuView['en-savoir-plus']['en']['titre raison 5']['contenu'];
+                    $tempId = $contenu['en-savoir-plus']['en']['titre raison 5']['id'];
+                    $language = 'en';
+                ?>
+                <?php include('repeat.php'); ?>
+                <?php
+                    $tempContenu = $contenu['en-savoir-plus']['en']['raison 5']['contenu'];
+                    $tempContenuView = $contenuView['en-savoir-plus']['en']['raison 5']['contenu'];
+                    $tempId = $contenu['en-savoir-plus']['en']['raison 5']['id'];
+                    $language = 'en';
+                ?>
+                <?php include('repeat.php'); ?>
+            </div>
+        </div>
+    </div>
+    
+    
+</div>
