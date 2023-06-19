7ea614642d8d68af7dd2cf04ba206d0fe0930bb3
Enzobu <enzo.palermo@epsi.fr>
2023-06-19 15:28:21 +0200

    update social club/humour text

--- fr/social-club.php
+++ fr/social-club.php
@@ -27,24 +27,25 @@
                 <div class="valeurs-part1">
                     <div class="principe">
                         <p>
-                            <span class="yello-txt size-txt">Objectif</span> S'informer et agir pour soi ou collectivement pour réussir à transformer notre futur en un avenir viable.
+                            <span class="yello-txt size-txt">Objectif</span> Éduquer les étudiants Fle pour contribuer à transformer notre futur en un avenir viable.
                         </p>
                     </div>
                     <div class="principe">
                         <p>
-                            <span class="yello-txt size-txt">Comment</span> En étant membre du Social Club.
+                            <!-- <span class="yello-txt size-txt">Comment</span> En étant membre du Social Club. -->
+                            <span class="yello-txt size-txt">Comment</span> En proposant tous les mois des actualités, des challenges, des quiz, des jeux et des ateliers éducatifs.
                         </p>
                     </div>
                     <div class="principe">
                         <p>
-                            <span class="yello-txt size-txt">Pourquoi un réseau social</span> Créer du lien entre virtuoses du climat est ressenti comme nécessaire pour se motiver, 
-                            se soutenir pour contenir le réchauffement climatique à 1,5°C à l'horizon 2030 et au delà.
+                            <span class="yello-txt size-txt">Pourquoi un club</span> Pour favoriser l'émulation collective. Pour s'inspirer mutuellement ou agir ensemble, réuni.e.s par le 
+                            talent et la volonté de contenir le réchauffement climatique à 1,5°C à l'horizon 2030 et au delà.
                         </p>
                     </div>
                     <div class="principe">
                         <p>
-                            <span class="yello-txt size-txt">Se connecter</span> Pour rester proches à distance. Pour s'inspirer mutuellement ou agir ensemble, réuni.e.s par leur 
-                            talent et leur volonté de contenir le réchauffement climatique à 1,5°C à l'horizon 2030 et au delà.
+                            <span class="yello-txt size-txt">Quel impact ?</span> Réduire indirectement l'empreinte carbone des étudiants en augmentant le nombre d'éco-sensibilisés grâce aux AF.
+                            À la fin du mois, fraîchement diplômés du Club Climat, ils participent à faire grimper notre compteur à impact.
                         </p>
                     </div>
                 </div>
@@ -54,24 +55,25 @@
                 <div class="infos">
                     <div class="info">
                         <p>
-                            <span class="orange-txt size-txt font-family">Un fil d'actualité</span> pour s'informer sur les initiatives individuelles et collectives qui font avancer l'éducation de
-                            toutes et tous aux enjeux du climat; les événements importants, les pastilles historiques ou humoristiques autour sur climat.
+                            <span class="orange-txt size-txt font-family">Un fil d'actualité</span> Ensemble de nouvelles séléctionnées sur les avancées pour le Cimat : Chiffres clés, pastilles humoristiques ou historiques, 
+                            avis d'experts, articles et vidéos de presse ou simple témoignage de citoyen.
                         </p>
                     </div>
                     <div class="info">
                         <p>
-                            <span class="orange-txt size-txt font-family">Des actions</span> pour agir collectivement avec la possibilité de s'inscrire, seul ou en groupe, au niveau au niveau local, 
-                            national ou international.
+                            <span class="orange-txt size-txt font-family">Un parcours climat</span> Mesurer l'empreinte carbone et le niveau de connaissances du climat au début du parcours pour chaque étudiant, 
+                            les inviter à participer aux ateliers, aux jeux, aux quiz et aux challenges du mois pour s'éduquer en s'amusant. Clôturer le parcours par une nouvelle mesure de l'empreinte carbone et 
+                            du niveau des connaissances de chaque étudiant. Au terme du parcours climat, un diplôme est délivré.
                         </p>
                     </div>
                     <div class="info">
                         <p>
-                            <span class="orange-txt size-txt font-family">Un agenda</span> pour partager les événements importants et les actions du social club.
+                            <span class="orange-txt size-txt font-family">Un agenda</span> Pour visualiser les dates et s'inscrire aux jeux, quiz, challenges et ateliers du mois.
                         </p>
                     </div>
                     <div class="info">
                         <p>
-                            <span class="orange-txt size-txt font-family">Une connexion</span> aux autre virtuoses en fonction des centres d'intérêts communs.
+                            <span class="orange-txt size-txt font-family">Un club social</span> Qui invite chaque étudiant diplômé à devenir ambassadeur du climat pour préserver notre bien commun.
                         </p>
                     </div>
                 </div>
@@ -83,7 +85,7 @@
         <div class="humour">
             <h1 class="top_txt titre-humour">Club <span class="orange-txt size-txt2 txt-g font-family">Humour</span></h1>
             <a href="/fr/contact.php" class="en-savoir-plus">Pré-réservation</a>
-            <div><span class="orange-txt size-txt2 txt-g font-family">“L'émancipation des femmes & le climat”</span></div>
+            <div><span class="orange-txt size-txt2 txt-g font-family">“L'implication des femmes & le climat”</span></div>
 
             <div class="top-illustration">
                 <img src="/img/humour/illustration_humour.png" alt="Illustration humour">
@@ -93,20 +95,16 @@
                 <div class="valeurs-part1">
                     <div class="principe">
                         <p>
-                            <span class="yello-txt size-txt">Objectif</span><br><br>
-                            Comprendre que lutter contre le réchauffement climatique et l'émancipation des femmes sont liés. Encore trop peu de <span class="yello-txt">femmes sont décisionnaires</span>
-                            des trajectoires à donner à nos sociétés partout dans le monde.<br><br>
-                            Elles doivent avoir le pouvoir de diriger afin de décider et de veiller à préserver notre humanité. Elles doivent faire respecter les droits des Humains
-                            et de tous les vivants dans le monde. Ainsi, nos chances de contenir un réchauffement à 1,5°C d'ici 2030 et au delà se multiplieront.<br><br>
-                            Nous avons besoin du regard des femmes sur la conduite de notre futur à mener. Notre futur en dépend.
+                        <span class="yello-txt size-txt">Objectif</span><br><br>
+                        Les femmes ont la possibilité de s'impliquer dans la lutte contre le réchauffement climatique. Elles ont la possiblité de décider de notre futur. 
+                        Faut-il encore qu'elles se sentent légitimes.
                         </p>
                     </div>
                     <div class="principe">
                         <p>
-                            <span class="yello-txt size-txt">Comment</span><br><br>
-                            <span class="yello-txt">Spectacle humoristique</span> sur la corrélation entre l'impact du modèle de société patriarcale et le réchauffement climatique. Sous forme de <span class="yello-txt">conférence
-                            ponctuée de musique et de vidéos</span>, menée par une femme en crises. L'idée est de démontrer les bienfaits de l'émancipation des femmes pour notre
-                            futur, sans domination masculine, sans menace climatique et respectueux du Vivant.
+                            <span class="yello-txt size-txt">Comment</span> <br><br>
+                            <span class="yello-txt">Conférence humoristique</span> pour inviter les femmes à oser être soi, à dépasser les clichés et 
+                            s'autoriser à prendre part aux décisions politiques à venir.
                         </p>
                     </div>
                 </div>
