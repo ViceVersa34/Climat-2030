2e9652de6c6e1df5f5c0f0697e977a89135f9822
Enzobu <enzo.palermo@epsi.fr>
2023-06-20 16:59:17 +0200

    update expo text

--- fr/exposition.php
+++ fr/exposition.php
@@ -13,7 +13,7 @@
 
     <main>
 
-        <h1 class="top_txt">Balade artistique <span class="orange-txt size-txt2 txt-g font-family">“La grande histoire du climat”</span></h1>
+        <h1 class="top_txt">Pour les <span class="orange-txt size-txt2 txt-g font-family">Alliances Françaises</span></h1>
         <a href="/fr/contact.php" class="en-savoir-plus">Demande de devis</a>
 
         <div class="top-illustration">
@@ -24,13 +24,71 @@
             <div class="valeurs-part1">
                 <div class="principe">
                     <p>
-                        <span class="yello-txt size-txt">Objectif</span> Comprendre les origines historiques du réchauffement climatique et ses répercussions actuelles et à venir.
+                        <span class="yello-txt size-txt">Objectif</span> Compenser l'empreinte carbone des Alliances Françaises en participant au financement du premier projet de renaturation d'une fotêt primaire à conditon qu'une partie de la forêt soit prévue en France.
                     </p>
                 </div>
                 <div class="principe">
                     <p>
-                        <span class="yello-txt size-txt">Comment</span> Exposition de frises chronologiques expliquant les mécanismes historiques, politiques, économiques, scientifiques
-                        et artistiques qui ont conduit à notre situation actuelle où contenir le réchauffement climatique à 1,5°C d'ici 2030 et au delà est devenu une nécessité absolue.
+                        <span class="yello-txt size-txt">Choix et Impact attendu</span> Contribuer à porter l'ambition de l'association Francis Hallé Pour Une Forêt Primaire afin d'accélérer auprès de l'État Français et de l'Europe son projet de forestation afin que renaisse la biodiversité. 
+                        <!-- <span class="yello-txt size-txt">Impact attendu</span> Changer de paradigme. <br> Hubert Reeves a dit un jour : "L'Homme mène un combat contre la nature, 
+                        s'il gagne, il perd.". <br> En s'éduquant avec Climat-2030, l'Humain mènera un combat pour la nature. S'il perd (ses habitudes), il gagne. -->
+                    </p>
+                </div>
+                <div class="principe">
+                    <p>
+                        <span class="yello-txt size-txt">Comment</span> Investir dans la plantation d'arbres de Francis Hallé pour la forêt primaire en permettant aux Alliances Françaises de reverser à l'association FH 12€ pour chaque commande d'article ci-dessous.
+                    </p>
+                </div>
+                <div class="principe ml-principe">
+                    <p>
+                        <span class="orange-txt size-txt font-family">Balade en forêt</span> Installation artistique “La grande Histoire du Climat” à exposer au sein des AF à l'occasion de la semaine du climat, des COP de l'ONU...
+                    </p>
+                </div>
+                <div class="principe ml-principe">
+                    <p>
+                        <span class="orange-txt size-txt font-family">Panorama du Climat</span> Grandes frises panoramiques à installer au-dessus des tableaux blancs des salles de cours.
+                    </p>
+                </div>
+                <div class="principe ml-principe">
+                    <p>
+                        <span class="orange-txt size-txt font-family">Conférence</span> Conférence dédiée aux équipes et aux étudiants de AF de Francis Hallé sur le lien entre la botanique, la biodiversité des forêts primaires et les champignons.
+                    </p>
+                </div>
+            </div>
+        </div>
+
+        
+        <div class="separation"></div>
+
+
+        <h1 class="etudiant top_txt">Pour les <span class="orange-txt size-txt2 txt-g font-family">Étudiants</span></h1>
+        <a href="/fr/contact.php" class="en-savoir-plus">Demande de devis</a>
+
+        <div class="top-illustration">
+            <img src="/img/exposition/illustration_exposition.png" alt="Illustration exposition">
+        </div>
+
+        <div class="valeurs">
+            <div class="valeurs-part1">
+                <div class="principe">
+                    <p>
+                        <span class="yello-txt size-txt">Objectif</span> Compenser l'empreinte carbone des édutiants en leur permettant de participant au financement du premier projet de renaturation d'une fotêt primaire. Point d'orgue de ce projet, une partie de la forêt est prévue en France.
+                    </p>
+                </div>
+                <div class="principe">
+                    <p>
+                        <span class="yello-txt size-txt">Impact attendu</span> Changer de paradigme. <br> Hubert Reeves a dit un jour : "L'Homme mène un combat contre la nature, 
+                        s'il gagne, il perd.". <br> En s'éduquant avec Climat-2030, l'Humain mènera un combat pour la nature. S'il perd (ses habitudes), il gagne.
+                    </p>
+                </div>
+                <div class="principe">
+                    <p>
+                        <span class="yello-txt size-txt">Comment</span>  Faire rayonner l'association Francis Hallé Pour Une Forêt Primaire dans le monde entier en permettant au étudiants de devenant des ambassadeurs de bonne volonté de cette ONG. Pour chaque livret commandé, une partie sera reversé a HF.
+                    </p>
+                </div>
+                <div class="principe ml-principe">
+                    <p>
+                        <span class="orange-txt size-txt font-family">Livret</span> “Le Climat et moi”, livre de poche illustré.
                     </p>
                 </div>
             </div>
