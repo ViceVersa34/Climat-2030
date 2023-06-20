f36fc27e58c43a6c5ce24def0d29e75ec7ab61c2
Enzobu <enzo.palermo@epsi.fr>
2023-06-20 18:56:57 +0200

    add academie du climat to admin header

--- admin/php/header.php
+++ admin/php/header.php
@@ -1,7 +1,6 @@
 <div class="nav">
     <a href="/fr/" class="img-logo"><img src="/img/logoV2.00.png" alt="logo Climat-2030"></a>
     <ul class="nav-list" id="nav">
-        <li class="nav-item accueil"><a href="/admin/">Accueil</a></li>
         <li class="nav-item users"><a href="/admin/tables/users.php">Users</a></li>
         <li class="nav-item customers"><a href="/admin/tables/customers.php">Customers</a></li>
         <li class="nav-item contents">
@@ -12,6 +11,7 @@
                 <li class="sub-nav-item"><a href="/admin/tables/contents/header.php">header</a></li>
                 <li class="sub-nav-item"><a href="/admin/tables/contents/footer.php">footer</a></li>
                 <li class="sub-nav-item"><a href="/admin/tables/contents/en-savoir-plus.php">En savoir plus</a></li>
+                <li class="sub-nav-item"><a href="/admin/tables/contents/academie-du-climat.php">Academie Du Climat</a></li>
             </ul>
         </li>
         <li class="nav-item my-account"><a href="/admin/my-account.php">Mon compte</a></li>
