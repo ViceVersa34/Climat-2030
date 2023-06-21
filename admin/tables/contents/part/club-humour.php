e992cb2e1700b2b890df69075ef1a2e9af946c42
Enzobu <enzo.palermo@epsi.fr>
2023-06-21 10:38:48 +0200

    create club-humour.php (admin)

---
+++ admin/tables/contents/part/club-humour.php
@@ -0,0 +1,105 @@
+<div class="page-selector" id="index"></div>
+<h2 class="title-page">club Climat</h2>
+<div class="contents">
+    <div class="group-content">
+        <div class="content">
+            <div class="language">
+                <?php 
+                    $tempContenu = $contenu['club-humour']['fr']['titre 1']['contenu'];
+                    $tempContenuView = $contenuView['club-humour']['fr']['titre 1']['contenu'];
+                    $tempId = $contenu['club-humour']['fr']['titre 1']['id'];
+                    $language = 'fr';
+                ?>
+                <?php include('repeat.php'); ?>
+                <?php 
+                    $tempContenu = $contenu['club-humour']['fr']['titre 2']['contenu'];
+                    $tempContenuView = $contenuView['club-humour']['fr']['titre 2']['contenu'];
+                    $tempId = $contenu['club-humour']['fr']['titre 2']['id'];
+                    $language = 'fr';
+                ?>
+                <?php include('repeat.php'); ?>
+            </div>
+            <div class="language">
+                <?php 
+                    $tempContenu = $contenu['club-humour']['en']['titre 1']['contenu'];
+                    $tempContenuView = $contenuView['club-humour']['en']['titre 1']['contenu'];
+                    $tempId = $contenu['club-humour']['en']['titre 1']['id'];
+                    $language = 'en';
+                ?>
+                <?php include('repeat.php'); ?>
+                <?php 
+                    $tempContenu = $contenu['club-humour']['en']['titre 2']['contenu'];
+                    $tempContenuView = $contenuView['club-humour']['en']['titre 2']['contenu'];
+                    $tempId = $contenu['club-humour']['en']['titre 2']['id'];
+                    $language = 'en';
+                ?>
+                <?php include('repeat.php'); ?>
+            </div>            
+        </div>
+    </div>
+
+    <div class="group-content">
+        <div class="content">
+            <div class="language">
+                <?php 
+                    $tempContenu = $contenu['club-humour']['fr']['titre question 1']['contenu'];
+                    $tempContenuView = $contenuView['club-humour']['fr']['titre question 1']['contenu'];
+                    $tempId = $contenu['club-humour']['fr']['titre question 1']['id'];
+                    $language = 'fr';
+                ?>
+                <?php include('repeat.php'); ?>
+                <?php 
+                    $tempContenu = $contenu['club-humour']['fr']['question 1']['contenu'];
+                    $tempContenuView = $contenuView['club-humour']['fr']['question 1']['contenu'];
+                    $tempId = $contenu['club-humour']['fr']['question 1']['id'];
+                    $language = 'fr';
+                ?>
+                <?php include('repeat.php'); ?>
+                <?php 
+                    $tempContenu = $contenu['club-humour']['fr']['titre question 2']['contenu'];
+                    $tempContenuView = $contenuView['club-humour']['fr']['titre question 2']['contenu'];
+                    $tempId = $contenu['club-humour']['fr']['titre question 2']['id'];
+                    $language = 'fr';
+                ?>
+                <?php include('repeat.php'); ?>
+                <?php 
+                    $tempContenu = $contenu['club-humour']['fr']['question 2']['contenu'];
+                    $tempContenuView = $contenuView['club-humour']['fr']['question 2']['contenu'];
+                    $tempId = $contenu['club-humour']['fr']['question 2']['id'];
+                    $language = 'fr';
+                ?>
+                <?php include('repeat.php'); ?>
+            </div>
+            <div class="language">
+                <?php 
+                    $tempContenu = $contenu['club-humour']['en']['titre question 1']['contenu'];
+                    $tempContenuView = $contenuView['club-humour']['en']['titre question 1']['contenu'];
+                    $tempId = $contenu['club-humour']['en']['titre question 1']['id'];
+                    $language = 'en';
+                ?>
+                <?php include('repeat.php'); ?>
+                <?php 
+                    $tempContenu = $contenu['club-humour']['en']['question 1']['contenu'];
+                    $tempContenuView = $contenuView['club-humour']['en']['question 1']['contenu'];
+                    $tempId = $contenu['club-humour']['en']['question 1']['id'];
+                    $language = 'en';
+                ?>
+                <?php include('repeat.php'); ?>
+                <?php 
+                    $tempContenu = $contenu['club-humour']['en']['titre question 2']['contenu'];
+                    $tempContenuView = $contenuView['club-humour']['en']['titre question 2']['contenu'];
+                    $tempId = $contenu['club-humour']['en']['titre question 2']['id'];
+                    $language = 'en';
+                ?>
+                <?php include('repeat.php'); ?>
+                <?php 
+                    $tempContenu = $contenu['club-humour']['en']['question 2']['contenu'];
+                    $tempContenuView = $contenuView['club-humour']['en']['question 2']['contenu'];
+                    $tempId = $contenu['club-humour']['en']['question 2']['id'];
+                    $language = 'en';
+                ?>
+                <?php include('repeat.php'); ?>
+            </div>            
+        </div>
+    </div>
+</div>
