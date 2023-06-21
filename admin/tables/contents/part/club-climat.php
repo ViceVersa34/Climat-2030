b6c8d715c1d43360d7b505d03a8bb79a553f1132
Enzobu <enzo.palermo@epsi.fr>
2023-06-21 10:38:41 +0200

    create club-climat.php (admin)

---
+++ admin/tables/contents/part/club-climat.php
@@ -0,0 +1,156 @@
+<div class="page-selector" id="index"></div>
+<h2 class="title-page">club Climat</h2>
+<div class="contents">
+    <div class="group-content">
+        <div class="content">
+            <div class="language">
+                <?php 
+                    $tempContenu = $contenu['club-climat']['fr']['titre']['contenu'];
+                    $tempContenuView = $contenuView['club-climat']['fr']['titre']['contenu'];
+                    $tempId = $contenu['club-climat']['fr']['titre']['id'];
+                    $language = 'fr';
+                ?>
+                <?php include('repeat.php'); ?>
+            </div>
+            <div class="language">
+            <?php 
+                    $tempContenu = $contenu['club-climat']['en']['titre']['contenu'];
+                    $tempContenuView = $contenuView['club-climat']['en']['titre']['contenu'];
+                    $tempId = $contenu['club-climat']['en']['titre']['id'];
+                    $language = 'en';
+                ?>
+                <?php include('repeat.php'); ?>
+            </div>            
+        </div>
+    </div>
+
+    <div class="group-content">
+        <div class="content">
+            <div class="language">
+                <?php 
+                    $tempContenu = $contenu['club-climat']['fr']['question 1']['contenu'];
+                    $tempContenuView = $contenuView['club-climat']['fr']['question 1']['contenu'];
+                    $tempId = $contenu['club-climat']['fr']['question 1']['id'];
+                    $language = 'fr';
+                ?>
+                <?php include('repeat.php'); ?>
+                <?php 
+                    $tempContenu = $contenu['club-climat']['fr']['question 2']['contenu'];
+                    $tempContenuView = $contenuView['club-climat']['fr']['question 2']['contenu'];
+                    $tempId = $contenu['club-climat']['fr']['question 2']['id'];
+                    $language = 'fr';
+                ?>
+                <?php include('repeat.php'); ?>
+                <?php 
+                    $tempContenu = $contenu['club-climat']['fr']['question 3']['contenu'];
+                    $tempContenuView = $contenuView['club-climat']['fr']['question 3']['contenu'];
+                    $tempId = $contenu['club-climat']['fr']['question 3']['id'];
+                    $language = 'fr';
+                ?>
+                <?php include('repeat.php'); ?>
+                <?php 
+                    $tempContenu = $contenu['club-climat']['fr']['question 4']['contenu'];
+                    $tempContenuView = $contenuView['club-climat']['fr']['question 4']['contenu'];
+                    $tempId = $contenu['club-climat']['fr']['question 4']['id'];
+                    $language = 'fr';
+                ?>
+                <?php include('repeat.php'); ?>
+            </div>
+            <div class="language">
+                <?php 
+                    $tempContenu = $contenu['club-climat']['en']['question 1']['contenu'];
+                    $tempContenuView = $contenuView['club-climat']['en']['question 1']['contenu'];
+                    $tempId = $contenu['club-climat']['en']['question 1']['id'];
+                    $language = 'en';
+                ?>
+                <?php include('repeat.php'); ?>
+                <?php 
+                    $tempContenu = $contenu['club-climat']['en']['question 2']['contenu'];
+                    $tempContenuView = $contenuView['club-climat']['en']['question 2']['contenu'];
+                    $tempId = $contenu['club-climat']['en']['question 2']['id'];
+                    $language = 'en';
+                ?>
+                <?php include('repeat.php'); ?>
+                <?php 
+                    $tempContenu = $contenu['club-climat']['en']['question 3']['contenu'];
+                    $tempContenuView = $contenuView['club-climat']['en']['question 3']['contenu'];
+                    $tempId = $contenu['club-climat']['en']['question 3']['id'];
+                    $language = 'en';
+                ?>
+                <?php include('repeat.php'); ?>
+                <?php 
+                    $tempContenu = $contenu['club-climat']['en']['question 4']['contenu'];
+                    $tempContenuView = $contenuView['club-climat']['en']['question 4']['contenu'];
+                    $tempId = $contenu['club-climat']['en']['question 4']['id'];
+                    $language = 'en';
+                ?>
+                <?php include('repeat.php'); ?>
+            </div>            
+        </div>
+    </div>
+
+    <div class="group-content">
+        <div class="content">
+            <div class="language">
+                <?php 
+                    $tempContenu = $contenu['club-climat']['fr']['contenu 1']['contenu'];
+                    $tempContenuView = $contenuView['club-climat']['fr']['contenu 1']['contenu'];
+                    $tempId = $contenu['club-climat']['fr']['contenu 1']['id'];
+                    $language = 'fr';
+                ?>
+                <?php include('repeat.php'); ?>
+                <?php 
+                    $tempContenu = $contenu['club-climat']['fr']['contenu 2']['contenu'];
+                    $tempContenuView = $contenuView['club-climat']['fr']['contenu 2']['contenu'];
+                    $tempId = $contenu['club-climat']['fr']['contenu 2']['id'];
+                    $language = 'fr';
+                ?>
+                <?php include('repeat.php'); ?>
+                <?php 
+                    $tempContenu = $contenu['club-climat']['fr']['contenu 3']['contenu'];
+                    $tempContenuView = $contenuView['club-climat']['fr']['contenu 3']['contenu'];
+                    $tempId = $contenu['club-climat']['fr']['contenu 3']['id'];
+                    $language = 'fr';
+                ?>
+                <?php include('repeat.php'); ?>
+                <?php 
+                    $tempContenu = $contenu['club-climat']['fr']['contenu 4']['contenu'];
+                    $tempContenuView = $contenuView['club-climat']['fr']['contenu 4']['contenu'];
+                    $tempId = $contenu['club-climat']['fr']['contenu 4']['id'];
+                    $language = 'fr';
+                ?>
+                <?php include('repeat.php'); ?>
+            </div>
+            <div class="language">
+                <?php 
+                    $tempContenu = $contenu['club-climat']['en']['contenu 1']['contenu'];
+                    $tempContenuView = $contenuView['club-climat']['en']['contenu 1']['contenu'];
+                    $tempId = $contenu['club-climat']['en']['contenu 1']['id'];
+                    $language = 'en';
+                ?>
+                <?php include('repeat.php'); ?>
+                <?php 
+                    $tempContenu = $contenu['club-climat']['en']['contenu 2']['contenu'];
+                    $tempContenuView = $contenuView['club-climat']['en']['contenu 2']['contenu'];
+                    $tempId = $contenu['club-climat']['en']['contenu 2']['id'];
+                    $language = 'en';
+                ?>
+                <?php include('repeat.php'); ?>
+                <?php 
+                    $tempContenu = $contenu['club-climat']['en']['contenu 3']['contenu'];
+                    $tempContenuView = $contenuView['club-climat']['en']['contenu 3']['contenu'];
+                    $tempId = $contenu['club-climat']['en']['contenu 3']['id'];
+                    $language = 'en';
+                ?>
+                <?php include('repeat.php'); ?>
+                <?php 
+                    $tempContenu = $contenu['club-climat']['en']['contenu 4']['contenu'];
+                    $tempContenuView = $contenuView['club-climat']['en']['contenu 4']['contenu'];
+                    $tempId = $contenu['club-climat']['en']['contenu 4']['id'];
+                    $language = 'en';
+                ?>
+                <?php include('repeat.php'); ?>
+            </div>            
+        </div>
+    </div>
+</div>
