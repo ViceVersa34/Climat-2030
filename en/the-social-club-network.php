f6e7a2fd0b7a45d3cda647e1de1822446f26c265
Enzobu <enzo.palermo@epsi.fr>
2023-06-21 10:37:48 +0200

    social club db text

--- en/the-social-club-network.php
+++ en/the-social-club-network.php
@@ -15,8 +15,8 @@
     <main>
         <div class="social-network">
             <div class="repere social-club-repere" id="social-network"></div>
-            <h1 class="top_txt"><span class="orange-txt size-txt2 txt-g font-family">The Social Club </span>network</h1>
-            <a href="/en/contact.php" class="en-savoir-plus">Pre-booking</a>
+            <h1 class="top_txt"><?php echo html_entity_decode($contenuView['club-climat']['en']['titre']['contenu']); ?></span></h1>
+            <a href="/fr/contact.php" class="en-savoir-plus">Pré-inscription</a>
 
             <div class="top-illustration">
                 <img src="/img/social_club/casque.jpg" alt="Illustration social club">
@@ -25,26 +25,16 @@
             <div class="valeurs">
                 <div class="valeurs-part1">
                     <div class="principe">
-                        <p>
-                            <span class="yello-txt size-txt">Aim</span> Learn and act individually or collectively to succeed in metamorphosing our future into a sustainable one.
-                        </p>
+                        <p><?php echo html_entity_decode($contenuView['club-climat']['en']['question 1']['contenu']); ?></p>
                     </div>
                     <div class="principe">
-                        <p>
-                            <span class="yello-txt size-txt">How</span> Become a member of the Social Club.
-                        </p>
+                        <p><?php echo html_entity_decode($contenuView['club-climat']['en']['question 2']['contenu']); ?></p>
                     </div>
                     <div class="principe">
-                        <p>
-                            <span class="yello-txt size-txt">Why a social network</span> Create a link between climate virtuosos is felt as a necessity to motivate and support 
-                            each other to contain global warming to 1.5°C by 2030 and beyond.
-                        </p>
+                        <p><?php echo html_entity_decode($contenuView['club-climat']['en']['question 3']['contenu']); ?></p>
                     </div>
                     <div class="principe">
-                        <p>
-                            <span class="yello-txt size-txt">Connect</span> Stay close remotely. To inspire each other or act together, through the talents and willingness to 
-                            contain global warming to 1.5°C by 2030 and beyond.
-                        </p>
+                        <p><?php echo html_entity_decode($contenuView['club-climat']['en']['question 4']['contenu']); ?></p>
                     </div>
                 </div>
                 <div class="bottom-illustration">
@@ -52,36 +42,28 @@
                 </div>
                 <div class="infos">
                     <div class="info">
-                        <p>
-                            <span class="orange-txt size-txt font-family">News feed</span> to learn about individual and collective initiatives that make the education of all go 
-                            further about global warming issues; about important events, historical or humorous climate information.
-
-                        </p>
+                        <p><?php echo html_entity_decode($contenuView['club-climat']['en']['contenu 1']['contenu']); ?></p>
                     </div>
                     <div class="info">
-                        <p>
-                            <span class="orange-txt size-txt font-family">Actions</span> to act collectively with the possibility to register, alone or as a group, at the local, 
-                            national or international level.
-                        </p>
+                        <p><?php echo html_entity_decode($contenuView['club-climat']['en']['contenu 2']['contenu']); ?></p>
                     </div>
                     <div class="info">
-                        <p>
-                            <span class="orange-txt size-txt font-family">Agenda</span> to share important events and actions of the Social Club network.
-                        </p>
+                        <p><?php echo html_entity_decode($contenuView['club-climat']['en']['contenu 3']['contenu']); ?></p>
                     </div>
                     <div class="info">
-                        <p>
-                            <span class="orange-txt size-txt font-family">Connection</span> to other virtuosos according to their common interests.
-                        </p>
+                        <p><?php echo html_entity_decode($contenuView['club-climat']['en']['contenu 4']['contenu']); ?></p>
                     </div>
                 </div>
             </div>
             <div class="repere humour-repere" id="humour"></div>
         </div>
 
+        <div class="separation"></div>
+
         <div class="humour">
-            <h1 class="top_txt">Humour <span class="orange-txt size-txt2 txt-g font-family">“Women's Empowerment & Climate”</span></h1>
-            <a href="/en/contact.php" class="en-savoir-plus">Pre-booking</a>
+            <h1 class="top_txt titre-humour"><?php echo html_entity_decode($contenuView['club-humour']['en']['titre 1']['contenu']); ?></h1>
+            <a href="/fr/contact.php" class="en-savoir-plus">Pré-réservation</a>
+            <div><?php echo html_entity_decode($contenuView['club-humour']['en']['titre 2']['contenu']); ?></div>
 
             <div class="top-illustration">
                 <img src="/img/humour/illustration_humour.png" alt="Illustration humour">
@@ -89,24 +71,13 @@
 
             <div class="valeurs">
                 <div class="valeurs-part1">
-                    <div class="principe">
-                        <p>
-                            <span class="yello-txt size-txt">Goal</span><br><br>
-                            Understand that fighting global warming and women's empowerment are linked. Still too few  <span class="yello-txt">women are decision makers</span>
-                            of the trajectories to give to our societies all over the world.<br><br>
-                            They must have the power to lead in order to decide and ensure that our humanity is preserved. They must redefine the essential priorities and 
-                            ensure respect for the rights of human beings and all living beings in the world. This will increase our chances of containing 1.5°C warming by 
-                            2030 and beyond. <br><br>
-                            We need women to look at the conduct of our future. Our future depends on it.
-                        </p>
+                    <div class="principe club-humour">
+                        <div class="titre"><?php echo html_entity_decode($contenuView['club-humour']['en']['titre question 1']['contenu']); ?></div>
+                        <p><?php echo html_entity_decode($contenuView['club-humour']['en']['question 1']['contenu']); ?></p>
                     </div>
-                    <div class="principe">
-                        <p>
-                            <span class="yello-txt size-txt">How</span><br><br>
-                            <span class="yello-txt">Humorous show</span> about the link between the impact of the patriarchal model of society and global warming. The show looks like a 
-                            <span class="yello-txt">conference punctuated with music and videos</span>, led by a woman in crisis. The idea is to demonstrate the benefits of women's 
-                            emancipation for our future, without male domination, without climate threat and respectful of the Living.
-                        </p>
+                    <div class="principe club-humour">
+                        <div class="titre"><?php echo html_entity_decode($contenuView['club-humour']['en']['titre question 2']['contenu']); ?></div>
+                        <p><?php echo html_entity_decode($contenuView['club-humour']['en']['question 2']['contenu']); ?></p>
                     </div>
                 </div>
             </div>
