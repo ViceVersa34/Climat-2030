8a1f9e8f2a1083f7b69100009fbcfaa4a677bdcc
Enzobu <enzo.palermo@epsi.fr>
2023-06-21 10:36:13 +0200

    social club db text

--- fr/social-club.php
+++ fr/social-club.php
@@ -13,10 +13,9 @@
     </header>
 
     <main>
-
         <div class="social-network">
             <div class="repere social-club-repere" id="social-network"></div>
-            <h1 class="top_txt">Le Club <span class="orange-txt size-txt2 txt-g font-family">Climat</span></h1>
+            <h1 class="top_txt"><?php echo html_entity_decode($contenuView['club-climat']['fr']['titre']['contenu']); ?></span></h1>
             <a href="/fr/contact.php" class="en-savoir-plus">Pré-inscription</a>
 
             <div class="top-illustration">
@@ -26,27 +25,16 @@
             <div class="valeurs">
                 <div class="valeurs-part1">
                     <div class="principe">
-                        <p>
-                            <span class="yello-txt size-txt">Objectif</span> Éduquer les étudiants Fle pour contribuer à transformer notre futur en un avenir viable.
-                        </p>
+                        <p><?php echo html_entity_decode($contenuView['club-climat']['fr']['question 1']['contenu']); ?></p>
                     </div>
                     <div class="principe">
-                        <p>
-                            <!-- <span class="yello-txt size-txt">Comment</span> En étant membre du Social Club. -->
-                            <span class="yello-txt size-txt">Comment</span> En proposant tous les mois des actualités, des challenges, des quiz, des jeux et des ateliers éducatifs.
-                        </p>
+                        <p><?php echo html_entity_decode($contenuView['club-climat']['fr']['question 2']['contenu']); ?></p>
                     </div>
                     <div class="principe">
-                        <p>
-                            <span class="yello-txt size-txt">Pourquoi un club</span> Pour favoriser l'émulation collective. Pour s'inspirer mutuellement ou agir ensemble, réuni.e.s par le 
-                            talent et la volonté de contenir le réchauffement climatique à 1,5°C à l'horizon 2030 et au delà.
-                        </p>
+                        <p><?php echo html_entity_decode($contenuView['club-climat']['fr']['question 3']['contenu']); ?></p>
                     </div>
                     <div class="principe">
-                        <p>
-                            <span class="yello-txt size-txt">Quel impact ?</span> Réduire indirectement l'empreinte carbone des étudiants en augmentant le nombre d'éco-sensibilisés grâce aux AF.
-                            À la fin du mois, fraîchement diplômés du Club Climat, ils participent à faire grimper notre compteur à impact.
-                        </p>
+                        <p><?php echo html_entity_decode($contenuView['club-climat']['fr']['question 4']['contenu']); ?></p>
                     </div>
                 </div>
                 <div class="bottom-illustration">
@@ -54,27 +42,16 @@
                 </div>
                 <div class="infos">
                     <div class="info">
-                        <p>
-                            <span class="orange-txt size-txt font-family">Un fil d'actualité</span> Ensemble de nouvelles séléctionnées sur les avancées pour le Cimat : Chiffres clés, pastilles humoristiques ou historiques, 
-                            avis d'experts, articles et vidéos de presse ou simple témoignage de citoyen.
-                        </p>
+                        <p><?php echo html_entity_decode($contenuView['club-climat']['fr']['contenu 1']['contenu']); ?></p>
                     </div>
                     <div class="info">
-                        <p>
-                            <span class="orange-txt size-txt font-family">Un parcours climat</span> Mesurer l'empreinte carbone et le niveau de connaissances du climat au début du parcours pour chaque étudiant, 
-                            les inviter à participer aux ateliers, aux jeux, aux quiz et aux challenges du mois pour s'éduquer en s'amusant. Clôturer le parcours par une nouvelle mesure de l'empreinte carbone et 
-                            du niveau des connaissances de chaque étudiant. Au terme du parcours climat, un diplôme est délivré.
-                        </p>
+                        <p><?php echo html_entity_decode($contenuView['club-climat']['fr']['contenu 2']['contenu']); ?></p>
                     </div>
                     <div class="info">
-                        <p>
-                            <span class="orange-txt size-txt font-family">Un agenda</span> Pour visualiser les dates et s'inscrire aux jeux, quiz, challenges et ateliers du mois.
-                        </p>
+                        <p><?php echo html_entity_decode($contenuView['club-climat']['fr']['contenu 3']['contenu']); ?></p>
                     </div>
                     <div class="info">
-                        <p>
-                            <span class="orange-txt size-txt font-family">Un club social</span> Qui invite chaque étudiant diplômé à devenir ambassadeur du climat pour préserver notre bien commun.
-                        </p>
+                        <p><?php echo html_entity_decode($contenuView['club-climat']['fr']['contenu 4']['contenu']); ?></p>
                     </div>
                 </div>
             </div>
@@ -84,9 +61,9 @@
         <div class="separation"></div>
 
         <div class="humour">
-            <h1 class="top_txt titre-humour">Club <span class="orange-txt size-txt2 txt-g font-family">Humour</span></h1>
+            <h1 class="top_txt titre-humour"><?php echo html_entity_decode($contenuView['club-humour']['fr']['titre 1']['contenu']); ?></h1>
             <a href="/fr/contact.php" class="en-savoir-plus">Pré-réservation</a>
-            <div><span class="orange-txt size-txt2 txt-g font-family">“L'implication des femmes & le climat”</span></div>
+            <div><?php echo html_entity_decode($contenuView['club-humour']['fr']['titre 2']['contenu']); ?></div>
 
             <div class="top-illustration">
                 <img src="/img/humour/illustration_humour.png" alt="Illustration humour">
@@ -94,19 +71,13 @@
 
             <div class="valeurs">
                 <div class="valeurs-part1">
-                    <div class="principe">
-                        <p>
-                        <span class="yello-txt size-txt">Objectif</span><br><br>
-                        Les femmes ont la possibilité de s'impliquer dans la lutte contre le réchauffement climatique. Elles ont la possiblité de décider de notre futur. 
-                        Faut-il encore qu'elles se sentent légitimes.
-                        </p>
+                    <div class="principe club-humour">
+                        <div class="titre"><?php echo html_entity_decode($contenuView['club-humour']['fr']['titre question 1']['contenu']); ?></div>
+                        <p><?php echo html_entity_decode($contenuView['club-humour']['fr']['question 1']['contenu']); ?></p>
                     </div>
-                    <div class="principe">
-                        <p>
-                            <span class="yello-txt size-txt">Comment</span> <br><br>
-                            <span class="yello-txt">Conférence humoristique</span> pour inviter les femmes à oser être soi, à dépasser les clichés et 
-                            s'autoriser à prendre part aux décisions politiques à venir.
-                        </p>
+                    <div class="principe club-humour">
+                        <div class="titre"><?php echo html_entity_decode($contenuView['club-humour']['fr']['titre question 2']['contenu']); ?></div>
+                        <p><?php echo html_entity_decode($contenuView['club-humour']['fr']['question 2']['contenu']); ?></p>
                     </div>
                 </div>
             </div>
