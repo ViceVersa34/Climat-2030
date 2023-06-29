44cea3f8b27313c4ac0eda7ac32d7e5237175884
Enzobu <enzo.palermo@epsi.fr>
2023-06-29 15:05:58 +0200

    add sub-title + repere

--- en/the-social-club.php
+++ en/the-social-club.php
@@ -17,12 +17,20 @@
             <div class="repere social-club-repere" id="climate-club"></div>
             <h1 class="top_txt"><?php echo html_entity_decode($contenuView['club-climat']['en']['titre']['contenu']); ?></span></h1>
             <a href="/en/contact.php" class="en-savoir-plus"><?php echo html_entity_decode($contenuView['club-climat']['en']['en-savoir-plus']['contenu']); ?></a>
-
+            <div class="desc-club-climat">
+                <div class="size-txt"><?php echo html_entity_decode($contenuView['academie-du-climat']['fr']['programme 0']['contenu']); ?></div>
+                <div class="liens-club-climat">
+                    <a href="#presentation" class="lien-club-climat"><?php echo html_entity_decode($contenuView['club-climat']['en']['repere 1']['contenu']); ?></a>
+                    <a href="#parcours" class="lien-club-climat"><?php echo html_entity_decode($contenuView['club-climat']['en']['repere 2']['contenu']); ?></a>
+                </div>
+            </div>
             <div class="top-illustration">
                 <img src="<?php echo $images['club-climat']['illustration 1']['image']; ?>" alt="Illustration social club">
             </div>
 
             <div class="valeurs">
+                <div class="repere-climat repere-climat-top" id="presentation"></div>
+                <div class="titre-presentation"><?php echo html_entity_decode($contenuView['club-climat']['en']['titre presentation']['contenu']); ?></div>
                 <div class="valeurs-part1">
                     <div class="principe">
                         <p><?php echo html_entity_decode($contenuView['club-climat']['en']['question 1']['contenu']); ?></p>
@@ -40,6 +48,8 @@
                 <div class="bottom-illustration">
                     <img src="<?php echo $images['club-climat']['illustration 2']['image']; ?>" alt="Illustration social club">
                 </div>
+                <div class="repere-climat repere-climat-bottom" id="parcours"></div>
+                <div class="titre-parcours"><?php echo html_entity_decode($contenuView['club-climat']['en']['titre parcours']['contenu']); ?></div>
                 <div class="infos">
                     <div class="info">
                         <p><?php echo html_entity_decode($contenuView['club-climat']['en']['contenu 1']['contenu']); ?></p>
@@ -63,7 +73,7 @@
         <div class="humour">
             <h1 class="top_txt titre-humour"><?php echo html_entity_decode($contenuView['club-humour']['en']['titre 1']['contenu']); ?></h1>
             <a href="/en/contact.php" class="en-savoir-plus"><?php echo html_entity_decode($contenuView['club-climat']['en']['en-savoir-plus']['contenu']); ?></a>
-            <div><?php echo html_entity_decode($contenuView['club-humour']['en']['titre 2']['contenu']); ?></div>
+            <h2><?php echo html_entity_decode($contenuView['club-humour']['en']['titre 2']['contenu']); ?></h2>
 
             <div class="top-illustration">
                 <img src="<?php echo $images['club-humour']['illustration 1']['image']; ?>" alt="Illustration humour">
--- fr/club-social.php
+++ fr/club-social.php
@@ -17,12 +17,20 @@
             <div class="repere social-club-repere" id="club-climat"></div>
             <h1 class="top_txt"><?php echo html_entity_decode($contenuView['club-climat']['fr']['titre']['contenu']); ?></span></h1>
             <a href="/fr/contact.php" class="en-savoir-plus"><?php echo html_entity_decode($contenuView['club-climat']['fr']['en-savoir-plus']['contenu']); ?></a>
-
+            <div class="desc-club-climat">
+                <div class="size-txt"><?php echo html_entity_decode($contenuView['academie-du-climat']['fr']['programme 0']['contenu']); ?></div>
+                <div class="liens-club-climat">
+                    <a href="#presentation" class="lien-club-climat"><?php echo html_entity_decode($contenuView['club-climat']['fr']['repere 1']['contenu']); ?></a>
+                    <a href="#parcours" class="lien-club-climat"><?php echo html_entity_decode($contenuView['club-climat']['fr']['repere 2']['contenu']); ?></a>
+                </div>
+            </div>
             <div class="top-illustration">
                 <img src="<?php echo $images['club-climat']['illustration 1']['image']; ?>" alt="Illustration social club">
             </div>
 
             <div class="valeurs">
+                <div class="repere-climat repere-climat-top" id="presentation"></div>
+                <div class="titre-presentation"><?php echo html_entity_decode($contenuView['club-climat']['fr']['titre presentation']['contenu']); ?></div>
                 <div class="valeurs-part1">
                     <div class="principe">
                         <p><?php echo html_entity_decode($contenuView['club-climat']['fr']['question 1']['contenu']); ?></p>
@@ -40,6 +48,8 @@
                 <div class="bottom-illustration">
                     <img src="<?php echo $images['club-climat']['illustration 2']['image']; ?>" alt="Illustration social club">
                 </div>
+                <div class="repere-climat repere-climat-bottom" id="parcours"></div>
+                <div class="titre-parcours"><?php echo html_entity_decode($contenuView['club-climat']['fr']['titre parcours']['contenu']); ?></div>
                 <div class="infos">
                     <div class="info">
                         <p><?php echo html_entity_decode($contenuView['club-climat']['fr']['contenu 1']['contenu']); ?></p>
@@ -63,7 +73,7 @@
         <div class="humour">
             <h1 class="top_txt titre-humour"><?php echo html_entity_decode($contenuView['club-humour']['fr']['titre 1']['contenu']); ?></h1>
             <a href="/fr/contact.php" class="en-savoir-plus"><?php echo html_entity_decode($contenuView['club-humour']['fr']['en-savoir-plus']['contenu']); ?></a>
-            <div><?php echo html_entity_decode($contenuView['club-humour']['fr']['titre 2']['contenu']); ?></div>
+            <h2><?php echo html_entity_decode($contenuView['club-humour']['fr']['titre 2']['contenu']); ?></h2>
 
             <div class="top-illustration">
                 <img src="<?php echo $images['club-humour']['illustration 1']['image']; ?>" alt="Illustration humour">
