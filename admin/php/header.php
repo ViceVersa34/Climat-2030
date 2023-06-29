da83504effecfa615a4ba48bb618a96ea7527fb1
Enzobu <enzo.palermo@epsi.fr>
2023-06-29 09:50:04 +0200

    clean header.php admin

--- admin/php/header.php
+++ admin/php/header.php
@@ -7,18 +7,18 @@
             <a href="/admin/tables/contents/">Contents</a>
             <ul class="sub-menu">
                 <div class="div-sub-menu">
+                    <li class="sub-nav-item"><a href="/admin/tables/contents/header.php">Header</a></li>
+                    <li class="sub-nav-item"><a href="/admin/tables/contents/footer.php">Footer</a></li>
                     <li class="sub-nav-item"><a href="/admin/tables/contents/">Accueil</a></li>
-                    <li class="sub-nav-item"><a href="/admin/tables/contents/CGU.php">CGU</a></li>
-                    <li class="sub-nav-item"><a href="/admin/tables/contents/header.php">header</a></li>
-                    <li class="sub-nav-item"><a href="/admin/tables/contents/footer.php">footer</a></li>
-                    <li class="sub-nav-item"><a href="/admin/tables/contents/en-savoir-plus.php">En savoir plus</a></li>
-                    <li class="sub-nav-item"><a href="/admin/tables/contents/academie-du-climat.php">Academie Du Climat</a></li>
+                    <li class="sub-nav-item"><a href="/admin/tables/contents/academie-du-climat.php">Club Pédagogique</a></li>
                     <li class="sub-nav-item"><a href="/admin/tables/contents/club-climat.php">Club Climat</a></li>
                     <li class="sub-nav-item"><a href="/admin/tables/contents/club-humour.php">Club Humour</a></li>
                     <li class="sub-nav-item"><a href="/admin/tables/contents/club-foret-AF.php">Club foret - AF</a></li>
                     <li class="sub-nav-item"><a href="/admin/tables/contents/club-foret-etudiants.php">Club foret - Étudiants</a></li>
-                    <li class="sub-nav-item"><a href="/admin/tables/contents/contact.php">Contact</a></li>
+                    <li class="sub-nav-item"><a href="/admin/tables/contents/en-savoir-plus.php">En savoir plus</a></li>
+                    <li class="sub-nav-item"><a href="/admin/tables/contents/CGU.php">CGU</a></li>
                     <li class="sub-nav-item"><a href="/admin/tables/contents/eco-sensibilise.php">Éco-sensibilisé</a></li>
+                    <li class="sub-nav-item"><a href="/admin/tables/contents/contact.php">Contact</a></li>
                 </div>
             </ul>
         </li>
@@ -26,13 +26,13 @@
             <a href="/admin/tables/images/">Images</a>
             <ul class="sub-menu">
                 <div class="div-sub-menu">
+                    <li class="sub-nav-item"><a href="/admin/tables/images/header.php">Header</a></li>
+                    <li class="sub-nav-item"><a href="/admin/tables/images/footer.php">Footer</a></li>
                     <li class="sub-nav-item"><a href="/admin/tables/images/">Accueil</a></li>
                     <li class="sub-nav-item"><a href="/admin/tables/images/club-pedagogique.php">Club Pédagogique</a></li>
                     <li class="sub-nav-item"><a href="/admin/tables/images/club-climat.php">Club Climat</a></li>
                     <li class="sub-nav-item"><a href="/admin/tables/images/club-humour.php">Club Humour</a></li>
                     <li class="sub-nav-item"><a href="/admin/tables/images/club-foret.php">Club Foret</a></li>
-                    <li class="sub-nav-item"><a href="/admin/tables/images/header.php">Header</a></li>
-                    <li class="sub-nav-item"><a href="/admin/tables/images/footer.php">Footer</a></li>
                     <li class="sub-nav-item"><a href="/admin/tables/images/eco-sensibilise.php">Éco-sensibilisé</a></li>
                 </div>
             </ul>
