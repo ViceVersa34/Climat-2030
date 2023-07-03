0adce198d586e5260a82b5b328db872bc1f9f28c
Enzobu <enzo.palermo@epsi.fr>
2023-07-03 15:55:44 +0200

    add comments

--- js/main.js
+++ js/main.js
@@ -22,14 +22,25 @@ function changerLeft (gauche){
     var element2 = document.getElementById('body');
     var element3 = document.getElementById('blur');
 
+    // Récupérer la largeur de la fenêtre du navigateur
     var largeur = window.innerWidth;
+
+    // Récupérer les dimensions de la navBar
     var elem = document.getElementById('nav');
     var rect = elem.getBoundingClientRect();
+
+    // Calculer la largeur restante disponible après le déplacement de la navBar vers la gauche
     var cache = largeur-rect['width'];
     
+    // Modifier la propriété 'left' de la navBar pour la déplacer vers la gauche selon la valeur passée en argument
     element1.style.left=gauche;
+
+
+    // Appliquer des styles pour masquer le défilement vertical et horizontal du body
     element2.style.overflowY='hidden';
     element2.style.overflowX='hidden';
+
+    // Afficher l'élément 'blur' et lui attribuer une largeur égale à la largeur restante calculée précédemment
     element3.style.display='block';
     element3.style.width=cache+'px';
 }
@@ -38,20 +49,16 @@ function changerLeft2 (gauche){
     var element1= document.getElementById('nav');
     var element2 = document.getElementById('body');
     var element3 = document.getElementById('blur');
+
+    // Rétablir la position de la navBar en utilisant la valeur passée en argument
     element1.style.left=gauche;
+    
+    // Rétablir le comportement de défilement vertical et horizontal du body à l'état initial
     element2.style.overflowY='auto';
     element2.style.overflowX='auto';
+    
+    // Masquer l'élément 'blur'
     element3.style.display='none';
-}
-
-function tailleCache (){
-    var largeur = window.innerWidth;
-    var elem = document.getElementById('nav');
-    var rect = elem.getBoundingClientRect();
-    var cache = largeur-rect['width'];
-    alert(rect['width']);
-    alert(largeur);
-    alert(cache);
 }
 
 // -------------------------------------- </NAV-BAR RESPONSIVE> --------------------------------------
