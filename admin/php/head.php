c54c7ef59e0cba29bb0abea3f1081f425d7b653e
Enzobu <enzo.palermo@epsi.fr>
2023-07-04 11:20:23 +0200

    Update head.php

--- admin/php/head.php
+++ admin/php/head.php
@@ -12,5 +12,5 @@ wght@100;300;400;500;700;900&display=swap" rel="stylesheet">
 wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
 <link rel="stylesheet" href="/admin/css/main.css">
 <script type="text/javascript" src="https://code.jquery.com/jquery-1.12.0.min.js"></script>
-<script src="/js/main.js"></script>
+<script src="/admin/js/main.js"></script>
 <link rel="icon" href="/admin/img/admin.png">
