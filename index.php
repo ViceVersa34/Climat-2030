9467bfc1a7059286dfeaf241bbd6edbcd9928511
Enzobu <enzo.palermo@epsi.fr>
2023-07-07 13:12:48 +0200

    add comments /index.php

--- index.php
+++ index.php
@@ -1,7 +1,10 @@
 <?php
 
+// Inclure le fichier de configuration
 include 'configuration.php';
 
+// Récupérer le fichier correspondant à l'URL
 $file = getFileFromUrl();
 
+// Inclure le fichier récupéré
 include $file;
